52367a6125e7a69f2a499a80025d0fc3d5449071
Maarten A. Breddels <maartenbreddels@gmail.com>
2014-09-11 10:11:42 +0200

    exception dialog can now send an email with the report

---
+++ python/gavi/vaex/__init__.py
@@ -0,0 +1,3 @@
+
+__version__ = (0, 1, 7)
+__version_text__ = "Vaex v%d.%d.%d" % __version__
--- python/gavi/vaex/qt.py
+++ python/gavi/vaex/qt.py
@@ -37,15 +37,56 @@ def dialog_info(parent, title, msg):
 	QtGui.QMessageBox.information(parent, title, msg)
 	
 import traceback as tb
+import sys
+import gavi.vaex
+import smtplib
+from email.mime.text import MIMEText
+
+def email(text):
+	# Open a plain text file for reading.  For this example, assume that
+	msg = MIMEText(text)
+
+	msg['Subject'] = 'Error report for: ' +gavi.vaex.__version_text__
+	email_from = "vaex@astro.rug.nl"
+	email_to = "breddels@astro.rug.nl"
+	msg['From'] = email_to
+	msg['To'] = email_to
+
+	# Send the message via our own SMTP server, but don't include the
+	# envelope header.
+	s = smtplib.SMTP('mailhost.astro.rug.nl')
+	s.helo('fw1.astro.rug.nl')
+	s.sendmail(email_to, [email_to], msg.as_string())
+	s.quit()
+	
 def qt_exception(parent, exctype, value, traceback):
 	trace_lines = tb.format_exception(exctype, value, traceback)
 	trace = "".join(trace_lines)
 	text = """An unexpected error occured, you may press ok and continue, but the program might be unstable.
 	
 	""" + trace
+	report = gavi.vaex.__version_text__ + "\n" + text 
 	dialog = QtGui.QMessageBox(parent)
-	dialog.setText("Unexpected error: %s" % (exctype, ))
-	dialog.setInformativeText(text)
-	QtGui.QMessageBox.critical(parent, "Unexpected error: %r" % (value, ), text)
+	dialog.setText("Unexpected error: %s\nDo you want to continue" % (exctype, ))
+	#dialog.setInformativeText(text)
+	dialog.setDetailedText(text)
+	buttonSend = QtGui.QPushButton("Email report", dialog) 
+	buttonQuit = QtGui.QPushButton("Quit program", dialog) 
+	buttonContinue = QtGui.QPushButton("Continue", dialog) 
+	def exit(ignore=None):
+		print "exit"
+		sys.exit(1)
+	def _email(ignore=None):
+		email(report)
+	buttonQuit.clicked.connect(exit)
+	buttonSend.clicked.connect(_email)
+	dialog.addButton(buttonSend, QtGui.QMessageBox.YesRole)
+	dialog.addButton(buttonQuit, QtGui.QMessageBox.NoRole)
+	dialog.addButton(buttonContinue, QtGui.QMessageBox.YesRole)
+	dialog.setDefaultButton(buttonSend)
+	dialog.setEscapeButton(buttonContinue)
+	dialog.raise_()
+	dialog.exec_()
+	#QtGui.QMessageBox.critical(parent, "Unexpected error: %r" % (value, ), text)
 	
 	
