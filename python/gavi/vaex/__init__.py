b94cf911cd444a204f8bb0403d4ff6598cfbe48c
Maarten A. Breddels <maartenbreddels@gmail.com>
2014-10-31 16:08:33 +0100

    new versioning scheme

--- python/gavi/vaex/__init__.py
+++ python/gavi/vaex/__init__.py
@@ -11,6 +11,6 @@ __version_tuple__ = version.versiontuple
 __program_name__ = "vaex"
 __version__ = "%d.%d.%d" % __version_tuple__
 __release__ = version.versiontring[1:]
-__clean_release__ = "%d.%d.%d-%d" % (__version_tuple__ + (version.commits,))
+__clean_release__ = "%d.%d.%d" % (__version_tuple__)
 __full_name__ = __program_name__ + "-" + __release__
 __clean_name__ =  __program_name__ + "-" + __clean_release__
--- python/gavi/vaex/setversion.py
+++ python/gavi/vaex/setversion.py
@@ -12,13 +12,16 @@ hash = '{hash}'
 if __name__ == "__main__":
 	version = sys.argv[1]
 	f = file(os.path.join(os.path.dirname(__file__), "version.py"), "w")
-	m = re.match("v([0-9]+)\.([0-9]+)\.([0-9]+)-([0-9]+)-([\w]+)", version)
+	m = re.match("v([0-9]+)\.([0-9]+)-([0-9]+)-([\w]+)", version)
 	if m is None:
-		print "%s is not a valid version string, example: v1.2.16-10-gf6859db, where v1.2.16 should be the tag" % version
+		print "%s is not a valid version string, example: v1.2-10-gf6859db, where v1.2.16 should be the tag" % version
 		sys.exit(1)
 	groups = m.groups()
-	versionstuple =  tuple(map(int, groups[:3]))
-	commits = groups[3]
-	hash = groups[4]
+	commits = groups[2]
+	versionstuple =  list(map(int, groups[:2]))
+	versionstuple.append(int(commits))
+	versionstuple = tuple(versionstuple)
+	hash = groups[3]
+	version = ".".join(map(str,versionstuple)) + "-" + str(hash)
 	print >>f, template.format(versionstring=version, versionstuple=versionstuple, commits=commits, hash=hash)
 	f.close()
