cda679944b296a18d1cd6cdebd84825efa350886
Maarten A. Breddels <maartenbreddels@gmail.com>
2015-10-19 16:42:15 +0200

    minor

--- python/vaex/version.py
+++ python/vaex/version.py
@@ -1,6 +1,6 @@
 
-versiontring = '0.3.44-gc17001a'
-versiontuple = (0, 3, 44)
-commits = 44
-hash = 'gc17001a'
+versiontring = '0.3.49-ge58747e'
+versiontuple = (0, 3, 49)
+commits = 49
+hash = 'ge58747e'
 
