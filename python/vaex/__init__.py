6030acc609bddd7c6ad78aa5f3bc4af8b300b34f
maartenbreddels <maartenbreddels@gmail.com>
2015-10-20 22:02:20 +0200

    detect if we are running on rtd, if so, ignore errors on imports

--- python/vaex/__init__.py
+++ python/vaex/__init__.py
@@ -17,11 +17,19 @@ __clean_release__ = "%d.%d.%d" % (__version_tuple__)
 __full_name__ = __program_name__ + "-" + __release__
 __clean_name__ =  __program_name__ + "-" + __clean_release__
 
-import vaex.dataset
-#import vaex.plot
-from vaex.dataset import Dataset
-from vaex.remote import ServerRest
-del ServerRest, Dataset
+import os
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+try:
+	import vaex.dataset
+	#import vaex.plot
+	from vaex.dataset import Dataset
+	from vaex.remote import ServerRest
+	del ServerRest, Dataset
+except:
+	if not on_rtd:
+		raise
 
 def open(path, *args, **kwargs):
 	"""Open a dataset from file given by path
