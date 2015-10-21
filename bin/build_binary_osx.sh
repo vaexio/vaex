235ae2b69ecfaa74d998d83fe1bf0741d4344720
maartenbreddels <maartenbreddels@gmail.com>
2015-10-21 20:18:26 +0200

    try osx building

--- bin/build_binary_osx.sh
+++ bin/build_binary_osx.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-conda create -y --name vaex-osx-build numpy scipy pyqt matplotlib pyopengl h5py numexpr astropy tornado jupyter futures future
-source activate vaex-osx-build
-pip install py2app aplus
+#conda create -y --name vaex-osx-build numpy scipy pyqt matplotlib pyopengl h5py numexpr astropy tornado jupyter futures future
+#source activate vaex-osx-build
+#pip install py2app aplus
+python setup.py install
 python setup.py py2app
