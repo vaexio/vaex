bbba987509ecc39cab757a47d5ed817ac40b0612
maartenbreddels <maartenbreddels@gmail.com>
2015-11-15 20:55:14 +0100

    this script will install miniconda etc, and will make sure vaex can run

---
+++ bin/get_vaex.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# run like $ curl http://bit.ly/get_vaex | sh
+case "$OSTYPE" in
+  solaris*) echo "SOLARIS not supported" ;;
+  darwin*)  wget --continue https://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh -O miniconda.sh; ;;
+  linux*)   wget --continue https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh ;;
+  bsd*)     echo "BSD not supported" ;;
+  *)        echo "unknown: $OSTYPE not supported" ;;
+esac
+
+
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create -q -n vaex python=2.7 numpy scipy pyqt matplotlib pyopengl h5py numexpr astropy tornado jupyter
+source activate vaex
+pip install -r requirements.txt
+git clone https://github.com/maartenbreddels/vaex/
+cd vaex
+python setup.py install
+
+echo '# to run vaex, execute: '
+echo '# $ export PATH="$HOME/miniconda/bin:$PATH"; source activate vaex'
+echo '# $ vaex'
