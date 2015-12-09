75d50aa1ee44c2d68133ba4b5ecc5d35b8e0fbc7
Maarten A. Breddels <maartenbreddels@gmail.com>
2015-12-09 14:32:14 +0100

    does basic memory bandwidth testing

---
+++ bin/vaex_benchmark_memory.py
@@ -0,0 +1,38 @@
+import vaex as vx
+import sys
+import timeit
+import numpy as np
+
+
+progressbar = False
+fn = sys.argv[1]
+print(("opening", fn))
+dataset = vx.open(fn)
+#dataset = vx.open_many(fn)
+
+expressions = sys.argv[2:]
+print "subspace", expressions
+subspace = dataset(*expressions)
+byte_size = len(dataset) * len(expressions) * 8
+#sums = subspace.sum()
+
+limits = subspace.minmax()
+
+N = 5
+print "benchmarking histogram"
+expr = "subspace.histogram(limits, 256)"
+times = timeit.repeat(expr, setup="from __main__ import subspace, dataset, np, limits", repeat=5, number=N)
+print "minimum time", min(times)/N
+bandwidth = [byte_size/1024.**3/(time/N) for time in times]
+print "%f GiB/s" % max(bandwidth)
+
+#expr = "np.nansum(dataset.columns['x'])"
+print "benchmarking sum"
+expr = "subspace.sum()"
+times = timeit.repeat(expr, setup="from __main__ import subspace, dataset, np", repeat=5, number=N)
+print "minimum time", min(times)/N
+bandwidth = [byte_size/1024.**3/(time/N) for time in times]
+print "%f GiB/s" % max(bandwidth)
+
+
+
