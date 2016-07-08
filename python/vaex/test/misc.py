59f740e46c55979b1dc58b2a110c0b4303915e2b
Maarten A. Breddels <breddels@astro.rug.nl>
2016-07-08 15:28:59 +0200

    made py3 compatible and added unittest for it

--- python/vaex/test/misc.py
+++ python/vaex/test/misc.py
@@ -1,1 +1,34 @@
 __author__ = 'breddels'
+
+import unittest
+import vaex.image
+import numpy as np
+default_size = 2
+default_shape = (default_size, default_size)
+
+class TestImage(unittest.TestCase):
+    def test_blend(self):
+        black = vaex.image.background(default_shape, "black")
+        white = vaex.image.background(default_shape, "white")
+        transparant_black = black * 1
+        transparant_black[...,3] = 100
+        for mode in vaex.image.modes:
+            self.assert_(np.all(vaex.image.blend([black, white]) == white))
+            self.assert_(np.all(vaex.image.blend([white, black]) == black))
+
+            grey = vaex.image.blend([white, transparant_black], mode)
+
+            self.assert_(np.all(grey[...,0:3] < white[...,0:3]))
+            self.assert_(np.all(grey[...,0:3] > black[...,0:3]))
+
+    def test_pil(self):
+        white = vaex.image.background(default_shape, "white")
+        # not much to test
+        im = vaex.image.rgba_2_pil(white)
+        raw_data = vaex.image.pil_2_data(im)
+        url = vaex.image.rgba_to_url(white)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
