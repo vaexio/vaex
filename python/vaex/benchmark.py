bd17b7f2202d42a4cfb2f047154bf8a28f420769
Maarten A. Breddels <breddels@astro.rug.nl>
2016-08-22 20:01:54 +0200

    print out stats and --repeat as argument

--- python/vaex/benchmark.py
+++ python/vaex/benchmark.py
@@ -12,6 +12,7 @@ def main(argv):
 	parser.add_argument("-N", help="run each batch N times (default: %(default)s)", type=int, default=5)
 	parser.add_argument("-f", "--fraction", help="fraction of dataset to use (default: %(default)s)", default=1., type=float)
 	parser.add_argument("-b", "--buffer-size", default=vx.execution.buffer_size_default, help="buffer size per thread (default: %(default)s)", type=int)
+	parser.add_argument("-r", "--repeat", default=5, help="repeat benchmark X times (default: %(default)s)", type=int)
 	parser.add_argument("-c", "--cpu-count", default=vx.multithreading.thread_count_default, help="thread count/cpu count (default: %(default)s)", type=int)
 	parser.add_argument("filename", help="filename of dataset to use")
 	parser.add_argument("expressions", help="list of expressions to export (or all when empty)", nargs="*")
@@ -34,11 +35,12 @@ def main(argv):
 	#sums = subspace.sum()
 
 	limits = subspace.minmax()
+	print limits
 
 	N = args.N
 	print "benchmarking minmax"
 	expr = "subspace.minmax()"
-	times = timeit.repeat(expr, setup="from vaex.benchmark import subspace, dataset, np", repeat=5, number=N)
+	times = timeit.repeat(expr, setup="from vaex.benchmark import subspace, dataset, np", repeat=args.repeat, number=N)
 	print "minimum time", min(times)/N
 	bandwidth = [byte_size/1024.**3/(time/N) for time in times]
 	print "%f GiB/s" % max(bandwidth)
@@ -49,7 +51,7 @@ def main(argv):
 	print
 	print "benchmarking histogram"
 	expr = "subspace.histogram(limits, 256)"
-	times = timeit.repeat(expr, setup="from vaex.benchmark import subspace, dataset, np, limits", repeat=5, number=N)
+	times = timeit.repeat(expr, setup="from vaex.benchmark import subspace, dataset, np, limits", repeat=args.repeat, number=N)
 	print "minimum time", min(times)/N
 	bandwidth = [byte_size/1024.**3/(time/N) for time in times]
 	print "%f GiB/s" % max(bandwidth)
@@ -59,9 +61,10 @@ def main(argv):
 	print
 
 	#expr = "np.nansum(dataset.columns['x'])"
+	print "sum=", subspace.sum()
 	print "benchmarking sum"
 	expr = "subspace.sum()"
-	times = timeit.repeat(expr, setup="from vaex.benchmark import subspace, dataset, np", repeat=5, number=N)
+	times = timeit.repeat(expr, setup="from vaex.benchmark import subspace, dataset, np", repeat=args.repeat, number=N)
 	print "minimum time", min(times)/N
 	bandwidth = [byte_size/1024.**3/(time/N) for time in times]
 	print "%f GiB/s" % max(bandwidth)
