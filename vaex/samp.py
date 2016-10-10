1f0360647e60c911dd054bb03e23e91c8726d919
maartenbreddels <maartenbreddels@gmail.com>
2016-10-10 17:08:47 +0200

    download votable with basic auth

--- vaex/samp.py
+++ vaex/samp.py
@@ -116,3 +116,74 @@ class SampSingle(object):
 		logging.debug("got samp msg: table.load.votable with params: %s", self.params)
 		self.client.disconnect()
 		return self.params["url"]
+
+
+import getpass
+import requests
+from requests.auth import HTTPBasicAuth
+import astropy.io.votable
+
+try:
+	input = raw_input
+except:
+	pass
+# py2/3 comp
+try:
+    from StringIO import StringIO as BytesIO
+except ImportError:
+    from io import BytesIO
+
+def ask_cmd_line(username, password):
+	if not username:
+		username = input("Username:")
+	if not password:
+		password = getpass.getpass("Password:")
+	return username, password
+
+def fetch_votable(url, username=None, password=None, ask=ask_cmd_line):
+	done = False
+	auth = None
+	while not done:
+		if username and password:
+			auth = HTTPBasicAuth(username, password)
+		req = requests.get(url, auth=auth)
+		if req.status_code == 401:
+			result = ask(username, password)
+			if result:
+				username, password = result
+				auth = HTTPBasicAuth(username, password)
+			else:
+				return
+		else:
+			done = True
+	if req.status_code == 200:
+		f = BytesIO()
+		f.write(req.content)
+		return astropy.io.votable.parse_single_table(f)
+	else:
+		return req
+
+def single_table(username=None, password=None):
+	#from vaex.samp import SampSingle
+	#from astropy.table import Table
+	samp = SampSingle()
+	url = samp.wait_for_table()
+	import os
+	import tempfile
+	import requests
+
+	done = False
+	table = fetch_votable(url, username, password)
+	return table
+	# while not done:
+	# r = requests.get()
+	# tempdir = tempfile.mkdtemp()
+	# path = os.path.join(tempdir, "out.vot.gz")
+	# os.system("wget -c -O %s %s" % (path, url))
+	# parts = os.path.split(url)
+	# print("parts", parts)
+	# path2 = os.path.join(tempdir, "out.vot")
+	# os.system("gunzip %s > /dev/null; mv %s %s  > /dev/null" % (path, path, path2))
+	# print(path, path2)
+	# table = Table.read(path2, format="votable")
+	# return from_astropy_table(table)
