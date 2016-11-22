d9678dfedb2c97a979cc984c8a4eb4faffdb777a
maartenbreddels <maartenbreddels@gmail.com>
2016-11-22 15:33:41 +0100

    added extra tests to the default run

--- vaex/test/__init__.py
+++ vaex/test/__init__.py
@@ -5,8 +5,10 @@ def load_tests(loader, tests, pattern):
 	import vaex.test.dataset
 	import vaex.test.ui
 	import vaex.test.misc
+	import vaex.test.plot
+	import vaex.test.cmodule
 	print(loader, tests, pattern)
-	for module in [vaex.test.dataset, vaex.test.ui, vaex.test.misc]:
+	for module in [vaex.test.dataset, vaex.test.ui, vaex.test.misc, vaex.test.cmodule, vaex.test.plot]:
 		tests.addTests(unittest.TestLoader().loadTestsFromModule(module))
 	return tests
 
