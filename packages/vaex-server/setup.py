f0cb5370cf2210b4b36471f841c57e238ebcc701
maartenbreddels <maartenbreddels@gmail.com>
2017-10-01 19:49:31 +0200

    bigsplit: vaex-server package

---
+++ packages/vaex-server/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import sys, os, imp
+
+import sys, os, imp
+from setuptools import Extension
+
+dirname = os.path.dirname(__file__)
+path_version = os.path.join(dirname, "vaex/server/_version.py")
+version = imp.load_source('version', path_version)
+
+
+name        = 'vaex'
+author      = "Maarten A. Breddels",
+author_email= "maartenbreddels@gmail.com",
+license     = 'MIT'
+version     = version.__version__
+url         = 'https://www.github.com/maartenbreddels/vaex'
+install_requires_server = ["tornado>4.1", "cachetools"]
+
+setup(name=name+'-server',
+    version=version,
+    description='Visualization for vaex',
+    url=url,
+    author=author,
+    author_email=author_email,
+    install_requires=install_requires_server,
+    license=license,
+    packages=['vaex.server'],
+    zip_safe=False,)
