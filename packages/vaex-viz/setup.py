3ee6b37e94bf72fb07f9a16b90b1ff7a0e413520
maartenbreddels <maartenbreddels@gmail.com>
2017-10-01 18:52:34 +0200

    bigsplit: initial commit

---
+++ packages/vaex-viz/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import sys, os, imp
+
+import sys, os, imp
+from setuptools import Extension
+
+dirname = os.path.dirname(__file__)
+path_version = os.path.join(dirname, "vaex/viz/_version.py")
+version = imp.load_source('version', path_version)
+
+
+name        = 'vaex'
+author      = "Maarten A. Breddels",
+author_email= "maartenbreddels@gmail.com",
+license     = 'MIT'
+version     = version.__version__
+url         = 'https://www.github.com/maartenbreddels/vaex'
+install_requires_viz = ["vaex-core", "matplotlib>=1.3.1", "pillow"]
+
+setup(name=name+'-viz',
+    version=version,
+    description='Visualization for vaex',
+    url=url,
+    author=author,
+    author_email=author_email,
+    install_requires=install_requires_viz,
+    license=license,
+    packages=['vaex.viz'],
+    zip_safe=False,)
