7ff452e14132f70c72de43a7fb77e0c0f6b41c4b
maartenbreddels <maartenbreddels@gmail.com>
2017-10-05 15:31:50 +0200

    new/move: Dataset.viz.plot2d, is an alias for Dataset.plot

--- packages/vaex-viz/vaex/viz/__init__.py
+++ packages/vaex-viz/vaex/viz/__init__.py
@@ -1,1 +1,6 @@
-print("imported viz")
+import vaex.dataset
+from vaex.utils import InnerNamespace
+
+def add_namespace():
+    vaex.dataset.Dataset.viz = InnerNamespace({})
+    vaex.dataset.Dataset.viz._add(plot2d=vaex.dataset.Dataset.plot)
