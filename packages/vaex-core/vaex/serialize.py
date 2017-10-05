9465138a9043530eb06e6e245ecc417cd5e9fbd8
maartenbreddels <maartenbreddels@gmail.com>
2017-10-05 20:40:20 +0200

    initial commit: used for (de)serializing object using dicts/lists/json

---
+++ packages/vaex-core/vaex/serialize.py
@@ -0,0 +1,26 @@
+import vaex.utils
+from traitlets import *
+  
+registry = {}
+
+def register(cls):
+    registry[fullname(cls)] = cls
+    return cls
+
+def fullname(cls):
+    return cls.__module__ + '.' + cls.__name__
+
+def to_dict(obj):
+    return dict(cls=fullname(obj.__class__), state=obj.state_get())
+
+def from_dict(d):
+    cls_name = d['cls']
+    if cls_name not in registry:
+        raise ValueError('unknown class: ' + cls_name)
+    else:
+        obj = registry[cls_name]()
+        obj.state_set(d['state'])
+        return obj
+
+def can_serialize(object):
+    return fullname(object.__class__) in registry
