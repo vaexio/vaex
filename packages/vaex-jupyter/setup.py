ef4e0ef1dfeb7f29841a5d50b792c19c8b7637bd
maartenbreddels <maartenbreddels@gmail.com>
2017-12-14 18:48:11 +0100

    packages: split of vaex-jupyter (formerly vaex.notebook)

---
+++ packages/vaex-jupyter/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import sys, os, imp
+
+import sys, os, imp
+from setuptools import Extension
+
+dirname = os.path.dirname(__file__)
+path_version = os.path.join(dirname, 'vaex/jupyter/_version.py')
+version = imp.load_source('version', path_version)
+
+
+name        = 'vaex'
+author      = 'Maarten A. Breddels'
+author_email= 'maartenbreddels@gmail.com'
+license     = 'MIT'
+version     = version.__version__
+url         = 'https://www.github.com/maartenbreddels/vaex'
+install_requires_jupyter = ['vaex-core', 'vaex-viz', 'bqplot>=0.10.1', 'ipyvolume>=0.4', 'ipyleaflet', 'ipympl']
+
+setup(name=name+'-jupyter',
+    version=version,
+    description='Jupyter notebook and Jupyter lab support for vaex',
+    url=url,
+    author=author,
+    author_email=author_email,
+    install_requires=install_requires_jupyter,
+    license=license,
+    packages=['vaex.jupyter'],
+    zip_safe=False,
+    entry_points={'vaex.namespace': ['ml = vaex.jupyter:add_namespace'],
+                  'vaex.plugin': ['plot = vaex.jupyter.add_plugin']}
+    )
