57f9c05cf69c6dae91e5b17465533b9929a26723
Jovan <jv@roe.ac.uk>
2018-01-05 00:57:07 +0100

    Flaking vaex-server

--- packages/vaex-server/setup.py
+++ packages/vaex-server/setup.py
@@ -1,7 +1,6 @@
+import os
+import imp
 from setuptools import setup
-import sys, os, imp
-
-import sys, os, imp
 from setuptools import Extension
 
 dirname = os.path.dirname(__file__)
@@ -9,21 +8,21 @@ path_version = os.path.join(dirname, 'vaex/server/_version.py')
 version = imp.load_source('version', path_version)
 
 
-name        = 'vaex'
-author      = 'Maarten A. Breddels'
-author_email= 'maartenbreddels@gmail.com'
-license     = 'MIT'
-version     = version.__version__
-url         = 'https://www.github.com/maartenbreddels/vaex'
+name = 'vaex'
+author = 'Maarten A. Breddels'
+author_email = 'maartenbreddels@gmail.com'
+license = 'MIT'
+version = version.__version__
+url = 'https://www.github.com/maartenbreddels/vaex'
 install_requires_server = ['vaex-core', 'tornado>4.1', 'cachetools']
 
-setup(name=name+'-server',
-    version=version,
-    description='Webserver and client for vaex for a remote dataset',
-    url=url,
-    author=author,
-    author_email=author_email,
-    install_requires=install_requires_server,
-    license=license,
-    packages=['vaex.server'],
-    zip_safe=False,)
+setup(name=name + '-server',
+      version=version,
+      description='Webserver and client for vaex for a remote dataset',
+      url=url,
+      author=author,
+      author_email=author_email,
+      install_requires=install_requires_server,
+      license=license,
+      packages=['vaex.server'],
+      zip_safe=False,)
