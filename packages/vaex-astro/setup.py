8a88110c4d5bdd39415a9745e6bc71b15f31302f
maartenbreddels <maartenbreddels@gmail.com>
2018-01-06 19:45:56 +0100

    formatting: double to single quotes

--- packages/vaex-astro/setup.py
+++ packages/vaex-astro/setup.py
@@ -5,16 +5,16 @@ import sys, os, imp
 from setuptools import Extension
 
 dirname = os.path.dirname(__file__)
-path_version = os.path.join(dirname, "vaex/astro/_version.py")
+path_version = os.path.join(dirname, 'vaex/astro/_version.py')
 version = imp.load_source('version', path_version)
 
 name        = 'vaex'
-author      = "Maarten A. Breddels"
-author_email= "maartenbreddels@gmail.com"
+author      = 'Maarten A. Breddels'
+author_email= 'maartenbreddels@gmail.com'
 license     = 'MIT'
 version     = version.__version__
 url         = 'https://www.github.com/maartenbreddels/vaex'
-install_requires_astro = ["vaex-core>=0.1", "kapteyn"]
+install_requires_astro = ['vaex-core>=0.1', 'kapteyn']
 
 setup(name=name+'-astro',
     version=version,
