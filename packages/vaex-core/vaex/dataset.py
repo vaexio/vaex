f85d500737bdae6b4089758d75859a1ec9fcef75
Maarten Breddels <maartenbreddels@gmail.com>
2018-11-13 20:42:44 +0100

    new: ds.describe() for describing the dataset

--- packages/vaex-core/vaex/dataset.py
+++ packages/vaex-core/vaex/dataset.py
@@ -4056,6 +4056,24 @@ array([[ 53.54521742,  -3.8123135 ,  -0.98260511],
         from IPython import display
         display.display(display.HTML(self._head_and_tail_table(n)))
 
+    def describe(self, strings=True, virtual=True):
+        import pandas as pd
+        N = len(self)
+        columns = {}
+        for feature in self.get_column_names(strings=strings, virtual=virtual)[:]:
+            dtype = str(self.dtype(feature))
+            if self.dtype(feature).kind in ['S', 'U', '']:
+                columns[feature] = ((dtype, '--', '--', '--', '--', '--'))
+            else:
+                count = self.count(feature, delay=True)
+                mean = self.mean(feature, delay=True)
+                minmax = self.minmax(feature, delay=True)
+                self.execute()
+                count, mean, minmax = count.get(), mean.get(), minmax.get()
+                count = int(count)
+                columns[feature] = ((dtype, count, N-count, mean, minmax[0], minmax[1]))
+        return pd.DataFrame(data=columns, index=['dtype', 'count', 'missing', 'mean', 'min', 'max'])
+
     def cat(self, i1, i2):
         from IPython import display
         html = self._as_html_table(i1, i2)
