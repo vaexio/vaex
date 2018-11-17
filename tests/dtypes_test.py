337d142e0626042daaba48c09885b4a374738cbe
Jovan <jv@roe.ac.uk>
2018-11-17 14:30:28 +0100

    Splitting the dtype test into 2: one that tests only ds.dtype, and another that tests ds.dtypes()

--- tests/dtypes_test.py
+++ tests/dtypes_test.py
@@ -1,7 +1,8 @@
 from commont import *
 
 
-def test_dtypes(ds_local):
+def test_dtype(ds_local):
+  ds = ds_local
   assert ds.dtype(ds.x) == np.float64
   assert ds.dtype(ds.y) == np.float64
   assert ds.dtype(ds.m) == np.float64
@@ -11,6 +12,8 @@ def test_dtypes(ds_local):
   assert ds.dtype('name') == 'S25'
   assert ds.dtype('obj') == np.object
 
+def test_dtypes(ds_local):
+  ds = ds_local
   all_dtypes = np.array([np.float64, np.float64, np.float64, np.float64, np.int64, np.int64, 'S25', np.object])
   np.testing.assert_array_equal(ds.dtypes(columns=None), all_dtypes)
   some_dtypes = np.array([np.float64, np.int64, 'S25', np.object])
