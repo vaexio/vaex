f7c6992819916c990529c8b9f5c6a4d9dd96dc1b
Jovan <jv@roe.ac.uk>
2018-11-17 09:52:51 +0100

    Adding unit-tests for the .dtype() method and a proposal for dtypes().

---
+++ tests/dtypes_test.py
@@ -0,0 +1,17 @@
+from commont import *
+
+
+def test_dtypes(ds_local):
+  assert ds.dtype(ds.x) == np.float64
+  assert ds.dtype(ds.y) == np.float64
+  assert ds.dtype(ds.m) == np.float64
+  assert ds.dtype(ds.nm) == np.float64
+  assert ds.dtype(ds.mi) == np.int64
+  assert ds.dtype('ints') == np.int64
+  assert ds.dtype('name') == 'S25'
+  assert ds.dtype('obj') == np.object
+
+  all_dtypes = np.array([np.float64, np.float64, np.float64, np.float64, np.int64, np.int64, 'S25', np.object])
+  np.testing.assert_array_equal(ds.dtypes(columns=None), all_dtypes)
+  some_dtypes = np.array([np.float64, np.int64, 'S25', np.object])
+  np.testing.assert_array_equal(ds.dtypes(columns=['x', 'mi', 'name', 'obj']), some_dtypes)
