f4e86cb1305fc93f77dac3641bce35036168dd48
Jovan <jv@roe.ac.uk>
2018-11-18 22:17:05 +0100

    Updated the dtype_test unit-test.

--- tests/dtypes_test.py
+++ tests/dtypes_test.py
@@ -1,20 +1,15 @@
-from commont import *
+from common import *
 
 
 def test_dtype(ds_local):
   ds = ds_local
-  assert ds.dtype(ds.x) == np.float64
-  assert ds.dtype(ds.y) == np.float64
-  assert ds.dtype(ds.m) == np.float64
-  assert ds.dtype(ds.nm) == np.float64
-  assert ds.dtype(ds.mi) == np.int64
-  assert ds.dtype('ints') == np.int64
-  assert ds.dtype('name') == 'S25'
-  assert ds.dtype('obj') == np.object
+  for name in ds.column_names:
+      assert ds[name].values.dtype == ds.dtype(ds[name])
+
 
 def test_dtypes(ds_local):
   ds = ds_local
-  all_dtypes = np.array([np.float64, np.float64, np.float64, np.float64, np.int64, np.int64, 'S25', np.object])
+  all_dtypes = [np.float64, np.float64, np.float64, np.float64, np.int64, np.int64, 'S25', np.object]
   np.testing.assert_array_equal(ds.dtypes(columns=None), all_dtypes)
-  some_dtypes = np.array([np.float64, np.int64, 'S25', np.object])
+  some_dtypes = [np.float64, np.int64, 'S25', np.object]
   np.testing.assert_array_equal(ds.dtypes(columns=['x', 'mi', 'name', 'obj']), some_dtypes)
