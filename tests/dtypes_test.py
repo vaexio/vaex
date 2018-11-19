da91f170c106c46a0d858e887220bc691066cdaa
Jovan <jv@roe.ac.uk>
2018-11-19 18:17:01 +0100

    Update of the dtypes unit-test.

--- tests/dtypes_test.py
+++ tests/dtypes_test.py
@@ -4,12 +4,9 @@ from common import *
 def test_dtype(ds_local):
   ds = ds_local
   for name in ds.column_names:
-      assert ds[name].values.dtype == ds.dtype(ds[name])
+    assert ds[name].values.dtype == ds.dtype(ds[name])
 
 
 def test_dtypes(ds_local):
   ds = ds_local
-  all_dtypes = [np.float64, np.float64, np.float64, np.float64, np.int64, np.int64, 'S25', np.object]
-  np.testing.assert_array_equal(ds.dtypes(columns=None), all_dtypes)
-  some_dtypes = [np.float64, np.int64, 'S25', np.object]
-  np.testing.assert_array_equal(ds.dtypes(columns=['x', 'mi', 'name', 'obj']), some_dtypes)
+  assert (ds.dtypes.values == [ds[name].dtype for name in ds.column_names]).all()
