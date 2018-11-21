97fff171f6313423a8748bc32da3c0eccd6aa416
Jovan <jv@roe.ac.uk>
2018-11-21 21:10:52 +0100

    Update of the values_tests to test that the masked values are exposed when the .values property is used for DatasetArrays.

--- tests/values_test.py
+++ tests/values_test.py
@@ -5,11 +5,12 @@ from common import *
 def test_values(ds_local):
     ds = ds_local
 
-    assert ds['x'].values.tolist() == np.array(ds.evaluate('x')).tolist()
-    assert ds['name'].values.tolist() == np.array(ds.evaluate('name')).tolist()
+    assert ds['x'].values.tolist() == ds.evaluate('x').tolist()
+    assert ds['name'].values.tolist() == ds.evaluate('name').tolist()
     assert ds['obj'].values.data.tolist() == ds.evaluate('obj').data.tolist()
     assert ds[['x', 'y']].values.tolist() == np.array([ds.evaluate('x'), ds.evaluate('y')]).T.tolist()
     assert ds[['x', 'y']].values.shape == (len(ds), 2)
+    assert ds[['m','x']].values[7][0] == 77777
     # The missing values are included. This may not be the correct behaviour
     assert ds[['x', 'y', 'nm']].values.tolist(), np.array([ds.evaluate('x'), ds.evaluate('y'), ds.evaluate('nm')]).T.tolist()
 
