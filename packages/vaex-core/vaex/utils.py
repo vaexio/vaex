bc35672dfbd61bf185afe69ef70f2185056ec221
shareactor <yonatan@shareactor.io>
2018-11-29 12:15:39 +0100

    NumpyEncoder for json write
    Created a NumpyEncoder class to help with encoding json

--- packages/vaex-core/vaex/utils.py
+++ packages/vaex-core/vaex/utils.py
@@ -450,6 +450,31 @@ def read_json_or_yaml(filename):
         raise ValueError("file should end in .json or .yaml (not %s)" % ext)
 
 
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        elif isinstance(obj, np.bytes_):
+            return obj.decode('UTF-8')
+        elif isinstance(obj, bytes):
+            return str(obj, encoding='utf-8');
+
+        else:
+            return super(NumpyEncoder, self).default(obj)
+
+
+def write_json(filename, data):
+    base, ext = os.path.splitext(filename)
+    if ext == ".json":
+        with open(filename, "w") as f:
+            json.dump(data, f, indent=2, cls=NumpyEncoder)
+    else:
+        raise ValueError("file should end in .json or .yaml (not %s)" % ext)
+
 # from http://stackoverflow.com/questions/5121931/in-python-how-can-you-load-yaml-mappings-as-ordereddicts
 
 _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
