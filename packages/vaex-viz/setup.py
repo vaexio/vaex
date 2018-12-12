c16fd833f5070a4224e420cba8bd687791fdefaf
Maarten Breddels <maartenbreddels@gmail.com>
2018-12-12 22:06:42 +0100

    require vaex-core>=0.6

--- packages/vaex-viz/setup.py
+++ packages/vaex-viz/setup.py
@@ -13,7 +13,7 @@ author_email = "maartenbreddels@gmail.com"
 license = 'MIT'
 version = version.__version__
 url = 'https://www.github.com/maartenbreddels/vaex'
-install_requires_viz = ["vaex-core", "matplotlib>=1.3.1", "pillow"]
+install_requires_viz = ["vaex-core>=0.6", "matplotlib>=1.3.1", "pillow"]
 
 setup(name=name + '-viz',
       version=version,
