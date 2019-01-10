f654e895e48a1c5080ad9f84df9a2c31c7add64a
Teake Nutma <teake.nutma@gmail.com>
2019-01-10 15:44:35 +0100

    Manually import underscored symbols from .dataframe to .dataset (#171)
    `from X import *` doesn't import under underscored symbols, so we have
    to do this by hand. It would be  better to put this in .dataframe.__all__,
    but this is less work and .dataset is (about to be) deprecated I think.
    
    Fixes #170.

--- packages/vaex-core/vaex/dataset.py
+++ packages/vaex-core/vaex/dataset.py
@@ -1,4 +1,32 @@
 from .dataframe import *
+from .dataframe import (
+    # .dataframe imports from .utils
+    _ensure_strings_from_expressions,
+    _ensure_string_from_expression,
+    _ensure_list,
+    _is_limit,
+    _isnumber,
+    _issequence,
+    _is_string,
+    _parse_reduction,
+    _parse_n,
+    _normalize_selection_name,
+    _normalize,
+    _parse_f,
+    _expand,
+    _expand_shape,
+    _expand_limits,
+    _split_and_combine_mask,
+    # dataframe definitions
+    _ColumnConcatenatedLazy,
+    _doc_snippets,
+    _functions_statistics_1d,
+    _hidden,
+    _is_array_type_ok,
+    _is_dtype_ok,
+    _requires
+)
+
 # alias kept for backward compatibility
 Dataset = DataFrame
 DatasetLocal = DataFrameLocal
