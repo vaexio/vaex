bc357690339efc57f867f9485fd53fe5ca7b1965
Jovan <jv@roe.ac.uk>
2019-02-05 18:48:39 +0100

    Fixing the from_dict unit-test to account for the ordering of the dictionary.

--- tests/from_dict_test.py
+++ tests/from_dict_test.py
@@ -5,5 +5,5 @@ def test_from_dict():
     data = {'A': [1, 2, 3], 'B': ['a', 'b', 'c']}
     ds = vaex.from_dict(data)
     assert 'A' in ds.get_column_names()
-    assert ds[0][0] == 1
-    assert ds[0][1] == b'a'
+    assert ds['A'].values[0] == 1
+    assert ds['B'].values[2] == b'c'
