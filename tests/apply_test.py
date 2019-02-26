78fb49d59239c845c2e20bea19e6a9a32aa6943f
Maarten Breddels <maartenbreddels@gmail.com>
2019-02-26 09:49:34 +0100

    Update apply_test.py

--- tests/apply_test.py
+++ tests/apply_test.py
@@ -2,5 +2,5 @@ import vaex
 
 
 def test_apply():
-    ds = vaex.from_arrays(**{'A': [1, 2, 3]})
-    assert ds.A.apply(lambda x: x + 1).values.tolist() == [2, 3, 4]
+    ds = vaex.from_arrays(x=[1, 2, 3])
+    assert ds.x.apply(lambda x: x + 1).values.tolist() == [2, 3, 4]
