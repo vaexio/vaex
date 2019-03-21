36b591d2af9cbc0cfd4641b692fb10b56e082da1
Maarten Breddels <maartenbreddels@gmail.com>
2019-03-21 09:23:02 +0100

    fix cornercases

--- packages/vaex-core/vaex/formatting.py
+++ packages/vaex-core/vaex/formatting.py
@@ -1,15 +1,25 @@
 import numpy as np
 import numbers
+import six
+
 
 MAX_LENGTH = 40
 
+
 def _format_value(value):
-    if isinstance(value, (str, bytes)):
+    if isinstance(value, six.string_types):
+        value = str(value)
+    elif isinstance(value, bytes):
         value = repr(value)
     elif isinstance(value, np.ma.core.MaskedConstant):
         value = str(value)
+    if isinstance(value, (np.timedelta64, np.datetime64)):
+        value = str(value)
     elif not isinstance(value, numbers.Number):
         value = str(value)
+    if isinstance(value, float):
+        value = repr(value)
     if isinstance(value, (str, bytes)):
-        value = repr(value[:MAX_LENGTH-3])[:-1] + '...'
+        if len(value) > MAX_LENGTH:
+            value = repr(value[:MAX_LENGTH-3])[:-1] + '...'
     return value
--- tests/repr_test.py
+++ tests/repr_test.py
@@ -1,14 +1,18 @@
 from common import *
+import pandas as pd
+import datetime
 
 def test_repr_default(ds_local):
     ds = ds_local
     code = ds._repr_mimebundle_()['text/plain']
     assert 'x' in code
 
+
 def test_repr_html(ds_local):
     ds = ds_local
     code = ds._repr_html_()
     assert 'x' in code
+
 
 def test_mask(ds_local):
     ds = ds_local
@@ -20,9 +24,11 @@ def test_mask(ds_local):
     assert "'--'" not in code
     assert "--" in code
 
+
 def test_repr_expression(ds_local):
     df = ds_local
     assert 'Error' not in repr(df.x)
+
 
 def test_repr_df_long_string():
     long_string = "Hi there" * 100
@@ -38,3 +44,23 @@ def test_repr_df_long_string():
     assert long_string not in str(df)
     assert long_string not in df._repr_html_()
     assert long_string not in df._as_html_table(0, 10)
+
+
+def test_repr_from_pandas():
+    dd_dict = {
+        'boolean': [True, True, False, None, True],
+        'text': ['This', 'is', 'some', 'text', 'so...'],
+        'numbers_1': [1, 30, -2, 1.5, 0.000],
+        'numbers_2': [1, None, -2, 1.5, 0.000],
+        'numbers_3': [1, np.nan, -2, 1.5, 0.000],
+        'time_1': [pd.NaT, datetime.date(2019, 1, 1), datetime.date(2019, 11, 1), datetime.date(2019, 1, 11), datetime.date(2019, 11, 11)],
+        'time_2': [pd.NaT, None, pd.NaT, pd.NaT, pd.NaT],
+    }
+
+    dd = pd.DataFrame(dd_dict)
+    dd['time_3'] = pd.to_timedelta(dd['time_2'] - dd['time_1'])
+    ds = vaex.from_pandas(dd)
+    repr_value = repr(ds)
+    str_value = str(ds)
+    assert 'NaT' in repr_value
+    assert 'NaT' in str_value
