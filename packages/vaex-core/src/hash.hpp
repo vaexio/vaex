a517d44091b1dc098bc1e8c551998f8637ba1230
Maarten Breddels <maartenbreddels@gmail.com>
2019-04-02 17:05:12 +0200

    hash: proper default template args

--- packages/vaex-core/src/hash.hpp
+++ packages/vaex-core/src/hash.hpp
@@ -1,10 +1,12 @@
 #include "flat_hash_map.hpp"
 #include "unordered_map.hpp"
+#include "tsl/hopscotch_set.h"
 
 namespace vaex {
 
-template<class K, class V>
-using hashmap = ska::flat_hash_map<K, V>;
+template<class Key, class Value, class Hash=std::hash<Key>, class Compare=std::equal_to<Key>>
+using hashmap = ska::flat_hash_map<Key, Value, Hash, Compare>;
 // using hashmap = tsl::hopscotch_map<K, V>;
-
+// template<class Key,  class Hash, class Compare>
+// using hashset = tsl::hopscotch_set<Key, Hash, Compare>;
 }
