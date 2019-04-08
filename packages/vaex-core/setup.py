13103fb02fd4dfead3f420dc0e8a06db09deb656
Maarten Breddels <maartenbreddels@gmail.com>
2019-04-08 12:49:32 +0200

    fix: include sys.prefix/include path for conda

--- packages/vaex-core/setup.py
+++ packages/vaex-core/setup.py
@@ -70,9 +70,14 @@ extension_vaexfast = Extension("vaex.vaexfast", [os.path.relpath(os.path.join(di
                                include_dirs=[get_numpy_include()],
                                extra_compile_args=extra_compile_args)
 extension_strings = Extension("vaex.strings", [os.path.relpath(os.path.join(dirname, "src/strings.cpp"))],
-                               include_dirs=[get_numpy_include(), get_pybind_include(),
-                               get_pybind_include(user=True), 'vendor/string-view-lite/include',
-                               'vendor/boost'],
+                               include_dirs=[
+                                   get_numpy_include(),
+                                   get_pybind_include(),
+                                   get_pybind_include(user=True),
+                                   'vendor/string-view-lite/include',
+                                   'vendor/boost',
+                                   os.path.join(sys.prefix, 'include')
+                                ],
                                extra_compile_args=extra_compile_args,
                                libraries=['pcre', 'pcrecpp']
                                )
