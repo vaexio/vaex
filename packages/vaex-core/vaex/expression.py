e491db92c0e4bf18091b5a5ca9edc17d18446a03
Maarten Breddels <maartenbreddels@gmail.com>
2019-04-28 20:36:29 +0200

    fix/new: support timedelta64 arithmetic

--- packages/vaex-core/vaex/expression.py
+++ packages/vaex-core/vaex/expression.py
@@ -92,6 +92,9 @@ class Meta(type):
                     if isinstance(b, Expression):
                         assert b.ds == a.ds
                         b = b.expression
+                    elif isinstance(b, (np.timedelta64)):
+                        df = a.ds
+                        b = df.add_variable('var_time_delta', b, unique=True)
                     expression = '({0} {1} {2})'.format(a.expression, op['code'], b)
                     return Expression(self.ds, expression=expression)
                 attrs['__%s__' % op['name']] = f
