0527b272fde0893ee945046615a32218e91375b9
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2019-06-17 19:06:11 +0200

    Updated the unit-test to include a masked array, and to also test against pandas methods.

--- tests/countna_test.py
+++ tests/countna_test.py
@@ -4,6 +4,11 @@ import numpy as np
 
 def test_countna():
     x = np.array([5, '', 1, 4, None, 6, np.nan, np.nan, 10, '', 0, 0, -13.5])
-    df = vaex.from_arrays(x=x)
+    y_data = np.array([np.nan, 2, None, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+    y_mask = np.array([0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1])
+    y = np.ma.MaskedArray(data=y_data, mask=y_mask)
+    df = vaex.from_arrays(x=x, y=y)
+    pandas_df = df.to_pandas_df()
 
-    assert df.x.countna() == 2
+    assert df.x.countna() == pandas_df.x.isna().sum()
+    assert df.y.countna() == pandas_df.y.isna().sum()
--- tests/isna_test.py
+++ tests/isna_test.py
@@ -4,13 +4,23 @@ import numpy as np
 
 def test_isna():
     x = np.array([5, '', 1, 4, None, 6, np.nan, np.nan, 10, '', 0, 0, -13.5])
-    df = vaex.from_arrays(x=x)
+    y_data = np.array([np.nan, 2, None, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+    y_mask = np.array([0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1])
+    y = np.ma.MaskedArray(data=y_data, mask=y_mask)
+    df = vaex.from_arrays(x=x, y=y)
+    pandas_df = df.to_pandas_df()
 
-    assert df.x.isna().tolist() == [False, False, False, False, False, False, True, True, False, False, False, False, False]
+    assert df.x.isna().tolist() == pandas_df.x.isna().tolist()
+    assert df.y.isna().tolist() == pandas_df.y.isna().tolist()
 
 
 def test_notna():
     x = np.array([5, '', 1, 4, None, 6, np.nan, np.nan, 10, '', 0, 0, -13.5])
-    df = vaex.from_arrays(x=x)
+    y_data = np.array([np.nan, 2, None, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+    y_mask = np.array([0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1])
+    y = np.ma.MaskedArray(data=y_data, mask=y_mask)
+    df = vaex.from_arrays(x=x, y=y)
+    pandas_df = df.to_pandas_df()
 
-    assert df.x.notna().tolist() == [True,  True,  True, True, True,  True, False, False, True, True, True, True, True]
+    assert df.x.notna().tolist() == pandas_df.x.notna().tolist()
+    assert df.y.notna().tolist() == pandas_df.y.notna().tolist()
