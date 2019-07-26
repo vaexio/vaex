c99591b88c8e26223b7d8dcfa8876a3a31b9a328
Maarten A. Breddels <maartenbreddels@gmail.com>
2019-07-26 09:53:12 +0200

    feat(core): accessors plugin system now allows lazy importing
    added an entry point {'vaex.dataframe.accessor': ['geo = vaex.geo:DataFrameAccessorGeo']}
    will only import vaex.geo when df.geo is accessed, this will speed up importing time of vaex.
    Also 'nested' accessors are supported, e.g.:
        entry_points={
            'vaex.dataframe.accessor': ['geo.cone = ectopylasm.vaex:ConeAccessor']
        }

---
+++ tests/internal/accessor_test.py
@@ -0,0 +1,47 @@
+import vaex
+import pytest
+
+
+class Foo(object):
+    def __init__(self, df):
+        self.df = df
+
+
+class Spam(object):
+    def __init__(self, df):
+        self.df = df
+
+
+class Egg(object):
+    def __init__(self, spam):
+        self.spam = spam
+        self.df = spam.df
+
+
+def test_accessor_basic():
+    vaex._add_lazy_accessor('foo', lambda: Foo)
+    df = vaex.example()
+    assert isinstance(df.foo, Foo)
+    assert df.foo is df.foo
+    assert df.foo.df is df
+
+def test_accessor_nested():
+    df = vaex.example()
+    vaex._add_lazy_accessor('spam.egg', lambda: Egg)
+    with pytest.raises(expected_exception=AttributeError):
+        a = df.spam
+    vaex._add_lazy_accessor('spam.egg.foo', lambda: Foo)
+    with pytest.raises(expected_exception=AttributeError):
+        a = df.spam
+    vaex._add_lazy_accessor('spam', lambda: Spam)
+    assert df.spam is df.spam
+    assert df.spam.df is df
+    assert isinstance(df.spam, Spam)
+
+    assert df.spam.egg is df.spam.egg
+    assert df.spam.egg.spam is df.spam
+    assert isinstance(df.spam.egg, Egg)
+
+    assert df.spam.egg.foo is df.spam.egg.foo
+    assert df.spam.egg.foo.df is df.spam.egg  # abuse of foo
+    assert isinstance(df.spam.egg.foo, Foo)
