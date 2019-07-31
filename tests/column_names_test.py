0247f0673c5c0473001b0b66adcbc716560536aa
Maarten Breddels <maartenbreddels@gmail.com>
2019-07-31 22:09:48 +0200

    feat(core, hdf5): support python keywords and invalid variable names (#370)
    * feat(core, hdf5): support python keywords and invalid variable names
    
    Implemented using a df._column_aliases dict that will translate those names
    when possible.
    Note that it means that you cannot do:
    df['class * 2']  # this expression cannot be parsed
    but instead you can do
    df['class'] * 2  # this can use the aliasing
    
    If for some reason you need to write an expression using strings, use:
    
    expression_string = str(df['class']) + ' * 2'

--- tests/column_names_test.py
+++ tests/column_names_test.py
@@ -1,17 +1,37 @@
 import vaex
 from common import *
 
-def test_column_names(ds_local):
-	ds = ds_local
-	columns_names = ds.get_column_names(virtual=True)
-	ds['__x'] = ds.x
-	assert columns_names == ds.get_column_names(virtual=True)
-	assert '__x' in ds.get_column_names(virtual=True, hidden=True)
-	assert len(columns_names) == len(ds.get_column_names(virtual=True, hidden=True))-1
 
-	ds = vaex.example()
-	ds['__x'] = ds['x'] + 1
-	assert 'FeH' in ds.get_column_names(regex='e*')
-	assert 'FeH' not in ds.get_column_names(regex='e')
-	assert '__x' not in ds.get_column_names(regex='__x')
-	assert '__x' in ds.get_column_names(regex='__x', hidden=True)
+x = np.arange(10)
+
+
+def test_column_names(ds_local):
+    ds = ds_local
+    columns_names = ds.get_column_names(virtual=True)
+    ds['__x'] = ds.x
+    assert columns_names == ds.get_column_names(virtual=True)
+    assert '__x' in ds.get_column_names(virtual=True, hidden=True)
+    assert len(columns_names) == len(ds.get_column_names(virtual=True, hidden=True))-1
+
+    ds = vaex.example()
+    ds['__x'] = ds['x'] + 1
+    assert 'FeH' in ds.get_column_names(regex='e*')
+    assert 'FeH' not in ds.get_column_names(regex='e')
+    assert '__x' not in ds.get_column_names(regex='__x')
+    assert '__x' in ds.get_column_names(regex='__x', hidden=True)
+
+
+def test_add_invalid_name(tmpdir):
+    # support invalid names and keywords
+    df = vaex.from_dict({'X!1': x, 'class': x*2})
+    assert df['X!1'].tolist() == x.tolist()
+    assert (df['X!1']*2).tolist() == (x*2).tolist()
+    assert (df['class']).tolist() == (x*2).tolist()
+    assert 'X!1' in df._column_aliases
+    assert (df.copy()['X!1']*2).tolist() == (x*2).tolist()
+
+    path = str(tmpdir.join('test.hdf5'))
+    df.export(path)
+    df = vaex.open(path)
+    assert df['X!1'].tolist() == x.tolist()
+    assert (df.copy()['X!1']*2).tolist() == (x*2).tolist()
