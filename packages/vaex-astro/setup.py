57e54ceaea25d9e136ab63a5d725155669c61e03
Maarten A. Breddels <maartenbreddels@gmail.com>
2019-08-23 09:34:49 +0200

    Release 0.6.1 of vaex-astro

--- packages/vaex-astro/setup.py
+++ packages/vaex-astro/setup.py
@@ -12,7 +12,7 @@ author_email= 'maartenbreddels@gmail.com'
 license     = 'MIT'
 version     = version.__version__
 url         = 'https://www.github.com/maartenbreddels/vaex'
-install_requires_astro = ['vaex-core>=0.1']
+install_requires_astro = ['vaex-core>=1.0.0,<2']
 
 setup(
     name=name + '-astro',
