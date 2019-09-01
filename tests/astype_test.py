1e4096db04e7821540db5e075802fd4c6efae3ff
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2019-09-01 08:27:00 +0200

    (core, test) Test for checking dtype of columns when astype and str methods are used together.

--- tests/astype_test.py
+++ tests/astype_test.py
@@ -10,3 +10,14 @@ def test_astype(ds_local):
 
     assert ds.x.evaluate().dtype == np.float32
     assert ds.x.tolist() == ds_original.x.evaluate().astype(np.float32).tolist()
+
+
+def test_astype_str():
+    df = vaex.from_arrays(x=['10,010', '-50,0', '11,111'])
+
+    df.rename_column(df.column_names[0], 'value')
+    df['value'] = df['value'].str.replace(',', '').evaluate()
+    df['value'] = (df['value'].astype('float')*1000).astype('int64').evaluate()
+
+    assert df.columns['value'].dtype == np.int64
+    assert df.value.dtype == np.int64
