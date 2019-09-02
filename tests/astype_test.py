6a1556acc2226a5e583282a14027392b28d87cb4
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2019-09-02 13:30:31 +0200

    (core, test) Testing dtype of columns when astype and str methods are used together. (#397)
    * fix(core): support reassigning (non virtual) columns + dtype checks
    
    Fixes #395

--- tests/astype_test.py
+++ tests/astype_test.py
@@ -10,3 +10,13 @@ def test_astype(ds_local):
 
     assert ds.x.evaluate().dtype == np.float32
     assert ds.x.tolist() == ds_original.x.evaluate().astype(np.float32).tolist()
+
+
+def test_astype_str():
+    df = vaex.from_arrays(x=['10,010', '-50,0', '11,111'])
+
+    df['x'] = df['x'].str.replace(',', '').evaluate()
+    df['x'] = (df['x'].astype('float')).astype('int64').evaluate()
+
+    assert df.columns['x'].dtype == np.int64
+    assert df.x.dtype == np.int64
