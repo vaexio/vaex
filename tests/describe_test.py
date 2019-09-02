0a59f251318806665e328b8d5d58515894f46cd7
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2019-09-02 16:26:19 +0200

    (core, test) Updating unit-test of describe to detect pd.NaT values.

--- tests/describe_test.py
+++ tests/describe_test.py
@@ -1,4 +1,5 @@
 from common import *
+import pandas as pd
 
 
 def test_describe(ds_local):
@@ -10,9 +11,16 @@ def test_describe(ds_local):
 def test_describe_NA():
     x = np.array([5, '', 1, 4, None, 6, np.nan, np.nan, 10, '', 0, 0, -13.5])
     y = np.array([5, -2, 1, 4, 0, 6, np.nan, np.nan, 10, 42, 0, np.nan, -13.5])
-    df = vaex.from_arrays(x=x, y=y)
+    t = np.array([np.datetime64('2001'), np.datetime64('2005'), np.datetime64('2001'),
+                  np.datetime64('2002'), np.datetime64('2006'), np.datetime64('2009'),
+                  np.datetime64('2003'), np.datetime64('2007'), np.datetime64('2011'),
+                  np.datetime64('2004'), np.datetime64('2008'), np.nan,
+                  pd.NaT])
+
+    df = vaex.from_arrays(x=x, y=y, t=t)
 
     desc_df = df.describe()
 
     assert desc_df.x.loc['NA'] == 3
     assert desc_df.y.loc['NA'] == 3
+    assert desc_df.t.loc['NA'] == 2
