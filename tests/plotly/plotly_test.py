ff756a72f220c2e58c66afc4353598ddb4195421
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2019-09-19 18:22:16 +0200

    (plotly) improving the test coverage

--- tests/plotly/plotly_test.py
+++ tests/plotly/plotly_test.py
@@ -23,13 +23,25 @@ def test_scatter(color):
                            title='title')
 
 
+def test_scatter_multiple():
+    df_iris.plotly.scatter(x=[df_iris.sepal_length, df_iris.sepal_width],
+                           y=[df_iris.petal_length, df_iris.petal_width],
+                           tooltip_title=df_iris.class_,
+                           selection=['sepal_width > 0.5', 'petal_width < 0.5'],
+                           color=['red', 'green'],
+                           colorbar=True,
+                           title='title')
+
+
 @pytest.mark.parametrize('f', [None, 'log1p'])
+@pytest.mark.parametrize('n', [None, 'normalize'])
 @pytest.mark.parametrize('shape', [32, 128])
-def test_heatmap(shape, f):
+def test_heatmap(shape, f, n):
     df_example.plotly.heatmap(x=df_example.x,
                               y=df_example.y,
                               shape=shape,
                               colorbar_label='Colorbar Label',
                               colormap='jet',
                               f=f,
+                              n=n,
                               selection='vx > 0')
