8ed87ceb43185388eda1c48c9c5d0325f835c6fd
Maarten A. Breddels <maartenbreddels@gmail.com>
2019-09-20 16:07:12 +0200

    clean up, put template in vue file

--- packages/vaex-jupyter/vaex/jupyter/widgets.py
+++ packages/vaex-jupyter/vaex/jupyter/widgets.py
@@ -2,7 +2,11 @@ import ipyvuetify as v
 import ipywidgets as widgets
 import traitlets
 from traitlets import *
+from . import traitlets as vt
 
+def load_template(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
 
 class PlotTemplate(v.VuetifyTemplate):
     show_output = Bool(False).tag(sync=True)
@@ -119,6 +123,7 @@ class ProgressCircularNoAnimation(v.VuetifyTemplate):
 
 class ExpressionTextArea(v.Textarea):
     df = traitlets.Any()
+    valid = traitlets.Bool(True)
 
     @traitlets.default('v_model')
     def _v_model(self):
@@ -151,9 +156,13 @@ class ExpressionTextArea(v.Textarea):
         try:
             self.df.validate_expression(self.v_model)
         except Exception as e:
+            self.success_messages = None
             self.error_messages = str(e)
+            self.valid = False
             return
         self.error_messages = None
+        self.success_messages = "Looking good"
+        self.valid = True
         return True
 
 
@@ -229,7 +238,7 @@ class ColumnExpressionAdder(ColumnPicker):
 
     def vue_menu_click(self, data):
         value = getattr(self.component, self.target)
-        setattr(self.component, self.target, value + ' + ' + str({self.items[data]}))
+        setattr(self.component, self.target, value + ' + ' + str(self.items[data]))
 
 
 class ColumnSelectionAdder(ColumnPicker):
@@ -251,9 +260,11 @@ class SelectionEditor(v.VuetifyTemplate):
     @traitlets.default('components')
     def _components(self):
         return {'component-input': self.input, 'adder': self.adder}
+
     @traitlets.default('input')
     def _input(self):
         return ExpressionSelectionTextArea(df=self.df)
+
     @traitlets.default('adder')
     def _adder(self):
         return ColumnSelectionAdder(df=self.df, component=self.input)
@@ -280,171 +291,65 @@ class VirtualColumnEditor(v.VuetifyTemplate):
         return {'editor': self.editor, 'adder': self.adder}
     @traitlets.default('editor')
     def _editor(self):
-        return ExpressionTextArea(df=self.df)
+        return ExpressionTextArea(df=self.df, rows=1)
     @traitlets.default('adder')
     def _adder(self):
         return ColumnExpressionAdder(df=self.df, component=self.editor)
 
     template = traitlets.Unicode('''
-        <v-layout column>
+        <v-layout column style="position: relative">
             <v-text-field placeholder="e.g. mycolumn" label="Column name" v-model='column_name' prepend-icon='edit'>test</v-text-field>
             <editor></editor>
-            <v-layout pa-4>
-                <v-btn @click="add()">Add column</v-btn>
+            <div style="position: absolute; right: 20px; bottom: 30px; opacity: 0.8">
                 <adder></adder>
-            </v-layout>
+            </div>
         </v-layout>''').tag(sync=True)
 
-    def vue_add(self, data=None):
-        df[self.column_name] = self.editor.v_model
-        if self.on_close:
-            self.on_close()
+    def save_column(self):
+        if self.editor.valid:
+            self.df[self.column_name] = self.editor.v_model
+            if self.on_close:
+                self.on_close()
 
-#     def add_event
 
-def nice_type(df, name):
-    dtype = df.dtype(name)
-    type_map = {'i': 'integer', 'u': 'integer', 'f': 'float', 'b': 'boolean',
-               'M': 'date/time'}
-    if dtype == str:
-        return 'string'
-    else:
-        return type_map[dtype.kind]
-
-class _Dialog(v.VuetifyTemplate):
-    df = traitlets.Any()
-    dialog = traitlets.Bool(False).tag(sync=True)
-    content_main = traitlets.Any()
-    components = traitlets.Dict(None, allow_none=True).tag(sync=True, **widgets.widget.widget_serialization)
-
-    @traitlets.default('components')
-    def _components(self):
-        return {'content-main': self.content_main}
-#     tooltip = traitlets.Unicode('Add example expression based on column...').tag(sync=True)
-    template = traitlets.Unicode('''
-    <div class="text-xs-center">
-
-    <v-dialog
-      v-model="dialog"
-      width="500"
-      @keydown.stop
-    >
-      <template v-slot:activator="{ on }">
-        <v-btn
-          color="red lighten-2"
-          dark
-          v-on="on"
-        >
-          Click Me
-        </v-btn>
-      </template>
-
-      <v-card>
-        <v-card-title
-          class="headline grey lighten-2"
-          primary-title
-        >
-          Add a virtual column
-        </v-card-title>
-
-        <content-main></content-main>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
-          >
-            Close
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </div>
-
-        ''').tag(sync=True)
-
-class ColumnList(v.VuetifyTemplate):
-    df = traitlets.Any()
-    predefined_columns = traitlets.List([]).tag(sync=True)
-    dialog = traitlets.Any()
+class ColumnList(v.VuetifyTemplate, vt.ColumnsMixin):
+    column_filter = traitlets.Unicode('').tag(sync=True)
+    valid_expression = traitlets.Bool(False).tag(sync=True)
+    dialog_open = traitlets.Bool(False).tag(sync=True)
     editor = traitlets.Any()
-    items = traitlets.List([{'name': 'foo', 'meta': 'integer'}, {'name': 'bar', 'meta': 'string'}]).tag(sync=True)
+    editor_open = traitlets.Bool(False).tag(sync=True)
+    tooltip = traitlets.Unicode('Add example expression based on column...').tag(sync=True)
+    template = traitlets.Unicode(load_template('columnlist.vue')).tag(sync=True)
+
+    def __init__(self, df=None, **kwargs):
+        super(ColumnList, self).__init__(df=df, **kwargs)
+        traitlets.dlink((self.editor.editor, 'valid'), (self, 'valid_expression'))
+        self.editor.editor.on_event('keypress.enter', self._on_enter)
 
     @traitlets.default('editor')
     def _editor(self):
         editor = VirtualColumnEditor(df=self.df)
         return editor
 
-    @traitlets.default('dialog')
-    def _dialog(self):
-        dialog = _Dialog(content_main=self.editor)
-        display(dialog)
-        return dialog
+    @traitlets.default('components')
+    def _components(self):
+        return {'content-editor': self.editor}
 
-    @traitlets.default('items')
-    def _items(self):
-        df = self.df
-        column_names = df.get_column_names()
-        def meta(k):
-            info = nice_type(df, k)
-            if k in df.virtual_columns:
-                info += " <i>virtual column</i>"
-            return info
-        return [{'name': k, 'meta': meta(k)} for k in column_names]
-#     button_text = traitlets.Unicode('Add column').tag(sync=True)
-    tooltip = traitlets.Unicode('Add example expression based on column...').tag(sync=True)
-    template = traitlets.Unicode('''
-        <v-layout>
-            <v-list>
-                  <v-subheader class="grey lighten-4" style="height: 40px; position: relative">
-                      <v-menu offset-y>
-                        <template v-slot:activator="{ on: menu }">
-                            <v-btn
-                                absolute bottom right
-                                color="primary"
-                                v-on="{...menu}" fab small>
-                                <v-icon>add</v-icon>
-                            </v-btn>
-                        </template>
-                        <v-list>
-                            <v-list-item @click="add_virtual_column()">
-                                <v-list-item-title>Add virtual column</v-list-item-title>
-                            </v-list-item>
-                            <v-list-item
-                                    v-for="(item, index) in predefined_columns"
-                                    :key="index"
-                                    @click="menu_click(index)">
-                                <v-list-item-title>{{ item }}</v-list-item-title>
-                            </v-list-item>
-                        </v-list>
-                    </v-menu>
-                      Columns
-                  </v-subheader>
-                    <v-list-item
-                        v-for="(item, index) in items"
-                        :key="index"
-                        @click="column_click(item)">
-                    <v-list-item-avatar>
-                            <v-icon class="grey white--text" v-if="!item.meta.includes('virtual')">view_column</v-icon>
-                            <v-icon  class="grey white--text"v-else>functions</v-icon>
-                    </v-list-item-avatar>
-                    <v-list-item-content>
-                        <v-list-item-title>{{ item.name }}</v-list-item-title>
-                        <v-list-item-subtitle v-html="item.meta"></v-list-item-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
+    def _on_enter(self, *ignore):
+        if self.valid_expression:
+            self.editor.save_column()
+            self.dialog_open = False
 
-        </v-layout>''').tag(sync=True)
     def vue_add_virtual_column(self, data):
-        self.dialog.dialog = True
+        self.dialog_open = True
+
+    def vue_save_column(self, data):
+        self.editor.save_column()
+        self.dialog_open = False
+
     def vue_column_click(self, data):
         name = data['name']
         if name in self.df.virtual_columns:
-            self.editor.v_model = self.df.virtual_columns[name]
+            self.editor.editor.v_model = self.df.virtual_columns[name]
             self.editor.column_name = name
-            self.dialog.dialog = True
+            self.dialog_open = True
