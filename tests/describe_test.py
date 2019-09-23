a9e0afc8d24736680ad01c1efe9ed4d616be94ef
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2019-09-23 14:19:43 +0200

    (test) refactor describe test for better clarity

--- tests/describe_test.py
+++ tests/describe_test.py
@@ -1,4 +1,5 @@
 from common import *
+
 import pandas as pd
 
 
@@ -9,18 +10,20 @@ def test_describe(ds_local):
 
 
 def test_describe_NA():
-    x = np.array([5, '', 1, 4, None, 6, np.nan, np.nan, 10, '', 0, 0, -13.5])
-    y = np.array([5, -2, 1, 4, 0, 6, np.nan, np.nan, 10, 42, 0, np.nan, -13.5])
-    t = np.array([np.datetime64('2001'), np.datetime64('2005'), np.datetime64('2001'),
-                  np.datetime64('2002'), np.datetime64('2006'), np.datetime64('2009'),
-                  np.datetime64('2003'), np.datetime64('2007'), np.datetime64('2011'),
-                  np.datetime64('2004'), np.datetime64('2008'), np.nan,
-                  pd.NaT])
-
-    df = vaex.from_arrays(x=x, y=y, t=t)
+    x = np.array([5, '', -1, 4.5, None, np.nan, np.nan])
+    y = np.array([0, 6, np.nan, np.nan, -13.13, -0.5, np.nan])
+    df = vaex.from_arrays(x=x, y=y)
 
     desc_df = df.describe()
 
     assert desc_df.x.loc['NA'] == 3
     assert desc_df.y.loc['NA'] == 3
+
+
+def test_describe_nat():
+    t = np.array([np.datetime64('2001'), pd.NaT, np.datetime64('2005'), np.nan])
+    df = vaex.from_arrays(t=t)
+
+    desc_df = df.describe()
+
     assert desc_df.t.loc['NA'] == 2
