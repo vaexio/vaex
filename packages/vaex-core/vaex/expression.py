86e249472769e4e9533c25b65ea18428155432c5
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2019-10-29 16:42:56 +0100

    feat(core) expressions can be materialized as pandas.Series

--- packages/vaex-core/vaex/expression.py
+++ packages/vaex-core/vaex/expression.py
@@ -227,6 +227,14 @@ class Expression(with_metaclass(Meta)):
         dsk[name] = self
         return da.Array(dsk, name, chunks, dtype=dtype)
 
+    def to_pandas_series(self):
+        """Return a pandas.Series representation of the expression.
+
+        Note: a memory copy is created.
+        """
+        import pandas as pd
+        return pd.Series(self.values)
+
     def __getitem__(self, slice):
         return self.ds[slice][self.expression]
 
