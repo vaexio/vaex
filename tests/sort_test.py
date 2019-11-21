833ac113b0ad46f512fa9221ba2f5a4c843e7291
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2019-11-21 10:23:43 +0100

    Enable multi-key sorting of a DataFrame (#463)
    * feat(core): Enable multi-key sorting

--- tests/sort_test.py
+++ tests/sort_test.py
@@ -26,3 +26,21 @@ def test_sort_filtered():
     df = vaex.from_arrays(x=x)
     df_sel_sorted = df[df.x > 4].sort(by='x')
     assert df_sel_sorted.x.tolist() == [5, 6, 7]
+
+def test_sort_multikey():
+    x = np.array([5, 3, 1, 1, 5])
+    y = np.array([0, 3, 4, 2, 1])
+    z = np.array(['dog', 'cat', 'cat', 'dog', 'mouse'])
+    df = vaex.from_arrays(x=x, y=y, z=z)
+
+    # Case 1: numeric keys
+    df_sorted_1 = df.sort(by=['x', 'y'])
+    assert df_sorted_1.x.tolist() == [1, 1, 3, 5, 5]
+    assert df_sorted_1.y.tolist() == [2, 4, 3, 0, 1]
+    assert df_sorted_1.z.tolist() == ['dog', 'cat', 'cat', 'dog', 'mouse']
+
+    # Case 2: str key followed by numeric key
+    df_sorted_2 = df.sort(by=['z', 'x'])
+    assert df_sorted_2.x.tolist() ==  [1, 3, 1, 5, 5]
+    assert df_sorted_2.y.tolist() ==  [4, 3, 2, 0, 1]
+    assert df_sorted_2.z.tolist() == ['cat', 'cat', 'dog', 'dog', 'mouse']
