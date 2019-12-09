153dde0f432a6352c20577537db8f9e6289aeb49
Maarten A. Breddels <maartenbreddels@gmail.com>
2019-12-09 15:22:05 +0100

    feat: have df.filter analogous to df.select for non-narrowing filtering

--- tests/selection_test.py
+++ tests/selection_test.py
@@ -1,6 +1,4 @@
-import numpy as np
-
-import vaex
+from common import *
 
 
 def test_selection_and_filter():
@@ -14,7 +12,18 @@ def test_selection_and_filter():
     df_filtered = df[df.x < 0]
     filtered_list = df_filtered['x'].tolist()
     assert filtered_list == selected_list
+    repr(df_filtered)
 
     # make sure we can slice, and repr
     df_sliced = df_filtered[:5]
-    repr(df_filtered)
+    repr(df_sliced)
+
+
+def test_filter(df):
+    dff = df[df.x>4]
+    assert dff.x.tolist() == list(range(5,10))
+
+    # vaex can have filters 'grow'
+    dff_bigger = dff.filter(dff.x < 3, mode="or")
+    dff_bigger = dff_bigger.filter(dff_bigger.x >= 0, mode="and")  # restore old filter (df_filtered)
+    assert dff_bigger.x.tolist() == list(range(3)) + list(range(5,10))
