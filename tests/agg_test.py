fa07c70a03e501bc7e991a79634146cec2fc53af
Maarten A. Breddels <maartenbreddels@gmail.com>
2019-12-10 15:01:22 +0100

    feat: pre_filter in aggregators to avoid evaluation of invalid data

--- tests/agg_test.py
+++ tests/agg_test.py
@@ -214,3 +214,16 @@ def test_upcast():
     assert df.f4.sum() == (2 + 1e-13)
 
     assert abs(df.b.var() - (0.2222)) < 0.01
+
+
+def test_agg_filtered_df_invalid_data():
+    # Custom function to be applied to a filtered DataFrame
+    def custom_func(x):
+        assert 4 not in x; return x**2
+
+    df = vaex.from_arrays(x=np.arange(10))
+    df_filtered = df[df.x!=4]
+    df_filtered.add_function('custom_function', custom_func)
+    df_filtered['y'] = df_filtered.func.custom_function(df_filtered.x)
+    # assert df_filtered.y.tolist() == [0, 1, 4, 9, 25, 36, 49, 64, 81]
+    assert df_filtered.count(df_filtered.y) == 9
