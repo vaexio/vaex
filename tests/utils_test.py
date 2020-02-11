36747761d136bf07664ee8e069f79e215305a2ca
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-02-11 14:40:50 +0100

    perf: use smaller datatypes for joining when possible (can safe 8x mem)

---
+++ tests/utils_test.py
@@ -0,0 +1,15 @@
+import vaex.utils
+import numpy as np
+import pytest
+
+
+def test_required_dtype_for_max():
+    assert vaex.utils.required_dtype_for_max(127, signed=True) == np.int8
+    assert vaex.utils.required_dtype_for_max(128, signed=True) == np.int16
+    assert vaex.utils.required_dtype_for_max(127, signed=False) == np.uint8
+    assert vaex.utils.required_dtype_for_max(128, signed=False) == np.uint8
+
+    assert vaex.utils.required_dtype_for_max(2**63-1, signed=True) == np.int64
+    assert vaex.utils.required_dtype_for_max(2**63, signed=False) == np.uint64
+    with pytest.raises(ValueError):
+        assert vaex.utils.required_dtype_for_max(2**63, signed=True) == np.int64
