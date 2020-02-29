10601e25069ee40224320872145a3182dbd926a8
Bulat Yaminov <bulat.yaminov@gmail.com>
2020-02-29 07:12:33 +0100

    fix(core): measure the impact of the faster type checks for dtype=object
    Before: it took 0:02:43.718827 to convert 12,748,986 rows (1.8 Gb), which is 77,871 rows per second
    After: it took 0:00:07.244279 to convert 12,748,986 rows (1.8 Gb), which is 1,759,869 rows per second

---
+++ benchmarks/from_pandas.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime
+
+import pandas as pd
+import vaex
+from vaex.datasets import nyctaxi_yellow_2015_jan
+
+# download and read Pandas CSV
+nyctaxi_yellow_2015_jan.download()
+csv_size = os.path.getsize(nyctaxi_yellow_2015_jan.filenames[0])
+df = pd.read_csv(nyctaxi_yellow_2015_jan.filenames[0])
+
+# convert to Vaex
+start = datetime.now()
+vdf = vaex.from_pandas(df)
+duration = datetime.now() - start
+
+print('it took {} to convert {:,} rows ({:.1f} Gb), which is {:,} rows per second'.format(
+    duration, len(df), csv_size / 1024. / 1024 / 1024, int(len(df) / duration.total_seconds())))
+
+# Last result when running on:
+# 2.8 GHz Quad-Core Intel Core i7; 16 GB 1600 MHz DDR3:
+# it took 0:00:07.244279 to convert 12,748,986 rows (1.8 Gb), which is 1,759,869 rows per second
