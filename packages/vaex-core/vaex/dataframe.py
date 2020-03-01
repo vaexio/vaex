82527e04f0e5d53dbeb87b3459251fbf1d4cec89
Bulat Yaminov <bulat.yaminov@gmail.com>
2020-03-01 17:49:34 +0100

    perf(core): even more efficient check for types in a column when creating a DataFrame

--- packages/vaex-core/vaex/dataframe.py
+++ packages/vaex-core/vaex/dataframe.py
@@ -2948,16 +2948,25 @@ class DataFrame(object):
                     ar_data = ar
                     if np.ma.isMaskedArray(ar):
                         ar_data = ar.data
-                    def is_missing(k):
-                        if k is None:
-                            return True
-                        try:
-                            # a way to detect NaN's and NaT
-                            return not (k == k)
-                        except:
-                            # if a value is an array, this will fail, and it is a non-missing
-                            return False
-                    types = list({type(k) for k in ar_data if not is_missing(k)})
+
+                    try:
+                        # "k != k" is a way to detect NaN's and NaT's
+                        types = list({type(k) for k in ar_data if k is not None and k == k})
+                    except ValueError:
+                        # If there is an array value in the column, Numpy throws a ValueError
+                        # "The truth value of an array with more than one element is ambiguous".
+                        # We don't handle this by default as it is a bit slower.
+                        def is_missing(k):
+                            if k is None:
+                                return True
+                            try:
+                                # a way to detect NaN's and NaT
+                                return not (k == k)
+                            except ValueError:
+                                # if a value is an array, this will fail, and it is a non-missing
+                                return False
+                        types = list({type(k) for k in ar_data if k is not is_missing(k)})
+
                     if len(types) == 1 and issubclass(types[0], six.string_types):
                         self._dtypes_override[valid_name] = str_type
                     if len(types) == 0:  # can only be if all nan right?
