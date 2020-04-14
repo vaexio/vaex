e0d2c894f941297e517055d5674daaa8beba368d
Bulat Yaminov <bulat.yaminov@gmail.com>
2020-04-14 22:59:23 +0200

    chore: setup ASV and convert Strings benchmark to ASV

---
+++ benchmarks/README.rst
@@ -0,0 +1,26 @@
+Running benchmarks
+------------------
+
+Vaex benchmarks are run with `Airspeed Velocity <https://asv.readthedocs.io/en/stable/>`__ on dedicated hardware. To develop or test benchmarks locally follow these steps:
+
+1. Install ASV:
+
+.. code:: bash
+
+    conda install -c conda-forge asv
+
+2. Implement or change the benchmarks next to existing ones. See `Writing benchmarks <https://asv.readthedocs.io/en/stable/writing_benchmarks.html>`__.
+
+3. Run them in dev mode: this will run them in the current Python environment and will repeat each test only once:
+
+.. code:: bash
+
+    asv dev
+
+4. Or run them fully on your laptop, again in the current Python environment:
+
+.. code:: bash
+
+    asv run --python=$(which python)
+
+On the dedicated hardware ASV will create a new Conda environment for each run.
--- benchmarks/strings.py
+++ benchmarks/strings.py
@@ -1,166 +1,119 @@
-"""
-Benchark ran on my laptop:
-python benchmarks/strings.py -n8 --npandas=6.5
-
-To benchmark on AWS, I did the following
- * spin up a large h1.8xlarge machine
-
-from the shell:
-
-sudo apt-get update
-sudo apt-get install g++
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-conda install -c conda-forge numpy scipy notebook matplotlib -y
-# if you want master
-# git clone --recursive https://github.com/vaexio/vaex/
-# cd vaex
-# pip install .
-pip install vaex-core vaex-hdf5 vaex-arrow 
-
-mkdir ramdisk
-sudo mount -t tmpfs -o size=50g tmpfs ramdisk
-
-python strings.py -n9 --npandas=6.5 --filename=ramdisk/test.hdf5 --partitions=100
-"""
-import timeit
-import vaex
-import numpy as np
-import dask.dataframe as dd
-import dask
-import json
-import sys
-import pandas as pd
 import os
-import time
-import argparse
-import multiprocessing
 
-filter_name = sys.argv[1] if len(sys.argv) > 1 else None
-
-default_filename = 'string_benchmark.hdf5'
-
-argv = sys.argv
-parser = argparse.ArgumentParser(argv[0])
-parser.add_argument('--number', "-n", dest="n", type=float, default=7, help="log number of rows to use")
-parser.add_argument('--nmax', type=int, default=9, help="number of rows for test dataset")
-parser.add_argument('--partitions', type=int, default=multiprocessing.cpu_count()*2, help="number of partitions to split (default: 2x number cores)")
-parser.add_argument('--npandas', dest="npandas", type=float, default=7, help="number of rows to use for pandas")
-parser.add_argument('--filter', dest="filter", default=None, help="filter for benchmark")
-parser.add_argument('--filename', default=default_filename, help='filename to use for benchmark export/reading')
-args = parser.parse_args(argv[1:])
-
-use_dask = False
+import numpy as np
+import vaex
 
 
-if not os.path.exists(args.filename):
-    x = np.arange(0, int(10**args.nmax))
-    xs = x.astype(str)
-    s = xs#vaex.string_column(xs)
-    df_vaex = vaex.from_arrays(x=s, s=s)
-    df_vaex.export(args.filename, progress=True, shuffle=True)
+class StringsSuite:
+    pretty_name = "Performance of string operations in DataFrames"
+    version = "1"
+    timeout = 3600  # in seconds
 
-df = vaex.open(args.filename)
-df_vaex = df[0:int(10**args.n)]
-df_vaex.executor.buffer_size = len(df_vaex)//args.partitions
-df_pandas = df[:int(10**args.npandas)].to_pandas_df()
+    param_names = [
+        'n',  # log number of rows to use
+        'partitions',  # number of partitions to split
+    ]
+    params = ([6], [24])
 
-if use_dask:
-    df_dask = dd.from_pandas(df_pandas, npartitions=4)
-timings = {}
-def mytimeit(expr, N, scope):
-    times = []
-    for i in range(N):
-        t0 = time.time()
-        eval(expr, scope)
-        times.append(time.time() - t0)
-    return times
-def test(name, expr):
-    if args.filter and args.filter not in name:
-        return
-    vaex_expr = expr + ".nop()"
-    N = 5
-    # t = timeit.Timer(vaex_expr, globals={'df': df_vaex})
-    # print(name, expr)
-    # t_vaex = min(t.repeat(3, N))/N/(10**args.n)
-    # print("\tvaex", t_vaex)
+    dataframe_nmax = 7  # log number of rows for test dataset
+    dataframe_filename = 'string_benchmark.hdf5'
 
-    t_vaex = min(mytimeit(vaex_expr, N, scope={'df': df_vaex}))/(10**args.n)
-    print(name, expr, '2')
-    print("\tvaex", t_vaex)
+    def setup_cache(self):
+        if not os.path.exists(self.dataframe_filename):
+            x = np.arange(0, int(10 ** self.dataframe_nmax))
+            s = x.astype(str)
+            df_vaex = vaex.from_arrays(x=s, s=s)
+            df_vaex.export(self.dataframe_filename, progress=True, shuffle=True)
 
-    # t = timeit.Timer(expr, globals={'df': df_pandas})
-    # t_pandas = min(t.repeat(3, N))/N/(10**args.npandas)
-    t_pandas = min(mytimeit(expr, N, scope={'df': df_pandas}))/(10**args.npandas)
-    print("\tpandas", t_pandas, t_pandas/t_vaex)
+    def setup(self, n, partitions):
+        df = vaex.open(self.dataframe_filename)
+        df_vaex = df[0:int(10 ** n)]
+        df_vaex.executor.buffer_size = len(df_vaex) // partitions
+        self.df = df_vaex
 
-    timings[name] = {'vaex': t_vaex, 'pandas': t_pandas}
+    def time_capitalize(self, n, partitions):
+        self.df.s.str.capitalize().nop()
 
-    if use_dask:
-        t_dask = min(mytimeit(expr+".compute()", N, scope={'df': df_dask}))/(10**args.npandas)
-        print("\tdask", t_dask)
-        timings[name]['dask'] = t_dask
+    def time_cat(self, n, partitions):
+        self.df.s.str.cat(self.df.s).nop()
 
-        with dask.config.set(scheduler='processes'):
-            t_dask = min(mytimeit(expr+".compute()", N, scope={'df': df_dask}))/(10**args.npandas)
-            print("\tdask_process", t_dask)
-            timings[name]['dask_process'] = t_dask
+    def time_contains(self, n, partitions):
+        self.df.s.str.contains("9", regex=False).nop()
 
-if __name__ == '__main__':
+    def time_contains_regex(self, n, partitions):
+        self.df.s.str.contains("9", regex=True).nop()
 
-    test('capitalize', 'df.s.str.capitalize()')
-    test('cat', 'df.s.str.cat(df.s)')
-    test('contains', 'df.s.str.contains("9", regex=False)')
-    test('contains(regex)', 'df.s.str.contains("9", regex=True)')
-    test('count', 'df.s.str.count("9")')
-    test('endswith', 'df.s.str.endswith("9")')
-    test('find', 'df.s.str.find("4")')
-    test('get', 'df.s.str.get(1)')
-    # test('index', 'df.s.str.index(1, 3)') TODO?
-    test('split+join', 'df.s.str.split(".").str.join("-")')
-    test('len', 'df.s.str.len()')
-    test('ljust', 'df.s.str.ljust(10)')
-    test('lower', 'df.s.str.lower()')
-    test('lstrip', 'df.s.str.lstrip("9")')
-    test('match', 'df.s.str.match("1.*")')
-    test('pad', 'df.s.str.pad(10)')
-    test('repeat', 'df.s.str.repeat(2)')
-    test('replace(default)', 'df.s.str.replace("123", "321")')
-    test('replace(no regex)', 'df.s.str.replace("123", "321", regex=False)')
-    test('replace(regex)', 'df.s.str.replace("1?[45]4", "1004", regex=True)')
-    test('rfind', 'df.s.str.rfind("4")')
-    test('rjust', 'df.s.str.rjust(10)')
-    test('rstrip', 'df.s.str.rstrip("9")')
-    test('slice', 'df.s.str.slice(1, 3)')
-    test('split', 'df.s.str.split(".")')
-    test('startswith', 'df.s.str.startswith("9")')
-    test('strip', 'df.s.str.strip("0")') # issues?
-    test('title', 'df.s.str.title()')
-    test('upper', 'df.s.str.upper()')
-    test('zfill', 'df.s.str.zfill(10)')
+    def time_count(self, n, partitions):
+        self.df.s.str.count("9").nop()
 
-    for name, values in timings.items():
-        tv = values['vaex']
-        tp = values['pandas']
-        print(name)
-        if use_dask:
-            td = values['dask']
-            print('\t', tv, tp, tp/tv, td/tv, tp/td)
-        else:
-            print('\t', tv, tp, tp/tv)
-   
-    previous_timings = {}
-    fn = "timings.json"
-    if os.path.exists(fn):
-        with open(fn, "r") as f:
-            previous_timings = json.load(f)
+    def time_endswith(self, n, partitions):
+        self.df.s.str.endswith("9").nop()
 
-    previous_timings.update(timings)
-    timings = previous_timings
-    print(timings)
-    fn = "timings.json"
-    with open(fn, "w") as f:
-        json.dump(timings, f)
-    print('write', fn)
+    def time_find(self, n, partitions):
+        self.df.s.str.find("4").nop()
+
+    def time_get(self, n, partitions):
+        self.df.s.str.get(1).nop()
+
+    def time_split_and_join(self, n, partitions):
+        self.df.s.str.split(".").str.join("-").nop()
+
+    def time_len(self, n, partitions):
+        self.df.s.str.len().nop()
+
+    def time_ljust(self, n, partitions):
+        self.df.s.str.ljust(10).nop()
+
+    def time_lower(self, n, partitions):
+        self.df.s.str.lower().nop()
+
+    def time_lstrip(self, n, partitions):
+        self.df.s.str.lstrip("9").nop()
+
+    def time_match(self, n, partitions):
+        self.df.s.str.match("1.*").nop()
+
+    def time_pad(self, n, partitions):
+        self.df.s.str.pad(10).nop()
+
+    def time_repeat(self, n, partitions):
+        self.df.s.str.repeat(2).nop()
+
+    def time_replace_default(self, n, partitions):
+        self.df.s.str.replace("123", "321").nop()
+
+    def time_replace_no_regex(self, n, partitions):
+        self.df.s.str.replace("123", "321", regex=False).nop()
+
+    def time_replace_regex(self, n, partitions):
+        self.df.s.str.replace("1?[45]4", "1004", regex=True).nop()
+
+    def time_rfind(self, n, partitions):
+        self.df.s.str.rfind("4").nop()
+
+    def time_rjust(self, n, partitions):
+        self.df.s.str.rjust(10).nop()
+
+    def time_rstrip(self, n, partitions):
+        self.df.s.str.rstrip("9").nop()
+
+    def time_slice(self, n, partitions):
+        self.df.s.str.slice(1, 3).nop()
+
+    def time_split(self, n, partitions):
+        self.df.s.str.split(".").nop()
+
+    def time_startswith(self, n, partitions):
+        self.df.s.str.startswith("9").nop()
+
+    def time_strip(self, n, partitions):
+        self.df.s.str.strip("0").nop()
+
+    def time_title(self, n, partitions):
+        self.df.s.str.title().nop()
+
+    def time_upper(self, n, partitions):
+        self.df.s.str.upper().nop()
+
+    def time_zfill(self, n, partitions):
+        self.df.s.str.zfill(10).nop()
