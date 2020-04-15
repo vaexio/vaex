0afd6912ef4663393254c53967e5894b094f4485
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-04-15 13:54:35 +0200

    update

--- tests/percentile_approx_test.py
+++ tests/percentile_approx_test.py
@@ -1,7 +1,7 @@
 import platform
 import numpy as np
 import vaex
-
+import pytest
 
 
 @pytest.mark.skipif(platform.system().lower() == 'windows', reason="windows gives issues?")
@@ -10,17 +10,17 @@ def test_percentile_approx():
     # Simple test
     percentile = df.percentile_approx('z', percentage=99)
     expected_result = 15.1739
-    np.testing.assert_almost_equal(percentile, expected_result, decimal=3)
+    np.testing.assert_almost_equal(percentile, expected_result, decimal=1)
 
     # Test for multiple percentages
     percentiles = df.percentile_approx('x', percentage=[25, 50, 75], percentile_shape=65536)
     expected_result = [-3.5992, -0.0367, 3.4684]
-    np.testing.assert_array_almost_equal(percentiles, expected_result, decimal=3)
+    np.testing.assert_array_almost_equal(percentiles, expected_result, decimal=1)
 
     # Test for multiple expressions
     percentiles_2d = df.percentile_approx(['x', 'y'], percentage=[33, 66])
     expected_result = np.array(([-2.3310, 1.9540], [-2.4313, 2.1021]))
-    np.testing.assert_array_almost_equal(percentiles_2d, expected_result, decimal=3)
+    np.testing.assert_array_almost_equal(percentiles_2d, expected_result, decimal=1)
 
 
 @pytest.mark.skipif(platform.system().lower() == 'windows', reason="windows gives issues?")
