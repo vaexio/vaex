cc641e7ff2fe3341f580ed72a655514a0920e20f
Maarten Breddels <maartenbreddels@gmail.com>
2020-04-15 10:34:23 +0200

    fix(expresso): support floor div, fixes #634 (#674)

---
+++ tests/expresso_test.py
@@ -0,0 +1,36 @@
+from vaex.expresso import parse_expression, node_to_string, simplify, translate
+
+
+def test_compare():
+    expr = "(x < 0)"
+    node = parse_expression(expr)
+    assert expr == node_to_string(node)
+
+    expr = "((x < 0) >= 6)"
+    node = parse_expression(expr)
+    assert expr == node_to_string(node)
+
+    expr = "(x // 10)"
+    node = parse_expression(expr)
+    assert expr == node_to_string(node)
+
+def test_simplify():
+    assert simplify("0 + 1") == "1"
+    assert simplify("1 + 0") == "1"
+    assert simplify("-0 + 1") == "1"
+    assert simplify("1 + -0") == "1"
+
+    assert simplify("0 * a + b") == "b"
+    assert simplify("a * 0 + b") == "b"
+    assert simplify("b + 0 * a") == "b"
+    assert simplify("b + a * 0") == "b"
+
+
+def test_lists():
+    node = parse_expression("searchsorted(['9E', 'AA', 'AQ', 'AS', 'B6', 'CO', 'DL', 'EV', 'F9', 'FL', 'HA', 'MQ', 'NW', 'OH', 'OO', 'UA', 'US', 'WN', 'XE', 'YV'], UniqueCarrier)")
+    assert node is not None
+
+    expr = "searchsorted(['9E', 'AA', 'AQ', 'AS', 'B6', 'CO', 'DL', 'EV', 'F9', 'FL', 'HA', 'MQ', 'NW', 'OH', 'OO', 'UA', 'US', 'WN', 'XE', 'YV'], UniqueCarrier)"
+    expr_translate = translate(expr, lambda x: None)
+    print(node)
+    assert expr == expr_translate
