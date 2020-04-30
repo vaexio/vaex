c3a1c852fdcea1f036ce18b270ca66093fd9b75f
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-04-30 22:12:42 +0200

    version file should be standalone

--- packages/vaex-core/vaex/core/_version.py
+++ packages/vaex-core/vaex/core/_version.py
@@ -1,4 +1,18 @@
+import os
+
 __version_tuple__ = (2, 0, 0, 'dev')
 __version__ = '2.0.0-dev'
-from vaex.version import patch_version
+
+nightly = bool(os.environ.get('VAEX_RELEASE_NIGHTLY', ''))
+
+def patch_version(version_tuple):
+    if nightly:
+        import datetime
+        dt = datetime.datetime.now()
+        date = (dt.year * 100 + dt.month)*100 + dt.day
+        version_tuple = (*version_tuple[:3], 'dev', date)
+    version_string = '.'.join(map(str, version_tuple[:4]))
+    if len(version_tuple) > 4:
+        version_string += str(version_tuple[-1])
+    return version_tuple, version_string
 __version_tuple__, __version__ = patch_version(__version_tuple__)
--- packages/vaex-core/vaex/version.py
+++ packages/vaex-core/vaex/version.py
@@ -1,21 +1,4 @@
-import os
 import pkg_resources
-
-
-nightly = bool(os.environ.get('VAEX_RELEASE_NIGHTLY', ''))
-
-
-def patch_version(version_tuple):
-    if nightly:
-        import datetime
-        dt = datetime.datetime.now()
-        date = (dt.year * 100 + dt.month)*100 + dt.day
-        version_tuple = (*version_tuple[:3], 'dev', date)
-    version_string = '.'.join(map(str, version_tuple[:4]))
-    if len(version_tuple) > 4:
-        version_string += str(version_tuple[-1])
-    return version_tuple, version_string
-
 
 packages = ['vaex', 'vaex-core', 'vaex-viz', 'vaex-hdf5', 'vaex-server', 'vaex-astro', 'vaex-ui', 'vaex-jupyter', 'vaex-ml', 'vaex-distributed', 'vaex-arrow', 'vaex-graphql']
 
