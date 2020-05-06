8c525fb750458891f12e32e37883e222a856af83
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-05-06 14:33:50 +0200

    feat(jupyter): linklist widget and more traits for card

---
+++ packages/vaex-jupyter/vaex/jupyter/vue/link-list.vue
@@ -0,0 +1,13 @@
+ <v-list dense class="pt-0">
+    <v-list-item v-for="item in items" :key="item.title" :href="item.url"
+                    target="_blank">
+        <v-list-item-action>
+            <v-icon v-if="item.icon">{{ item.icon }}</v-icon>
+            <img v-if="item.img" :src="item.img" style="height: 24px;"></img>
+        </v-list-item-action>
+
+        <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+    </v-list-item>
+</v-list>
