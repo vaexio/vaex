ae81a0f4be897ef118017a8f3015d4232d1fb0df
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-05-26 19:54:23 +0200

    chore: pin numpy to appearently compatible binary with wheels

--- packages/vaex-core/setup.py
+++ packages/vaex-core/setup.py
@@ -20,7 +20,7 @@ url = 'https://www.github.com/maartenbreddels/vaex'
 # TODO: can we do without requests and progressbar2?
 # TODO: after python2 supports frops, future and futures can also be dropped
 # TODO: would be nice to have astropy only as dep in vaex-astro
-install_requires_core = ["numpy>=1.11", "astropy>=2", "aplus", "tabulate>=0.8.3",
+install_requires_core = ["numpy>=1.16", "astropy>=2", "aplus", "tabulate>=0.8.3",
                          "future>=0.15.2", "pyyaml", "progressbar2", "psutil>=1.2.1",
                          "requests", "six", "cloudpickle", "pandas", "dask[array]", "nest-asyncio>=1.3.3"]
 if sys.version_info[0] == 2:
