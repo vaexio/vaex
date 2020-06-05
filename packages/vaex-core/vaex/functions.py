7a2c6295a86fd80a410b619a0a7457ba37648c8d
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-06-05 15:02:42 +0200

    fix(core): strftime was returning pandas Series, not a numpy array

--- packages/vaex-core/vaex/functions.py
+++ packages/vaex-core/vaex/functions.py
@@ -668,7 +668,7 @@ def dt_strftime(x, date_format):
     2  2015-11
     """
     import pandas as pd
-    return pd.Series(_pandas_dt_fix(x)).dt.strftime(date_format)
+    return pd.Series(_pandas_dt_fix(x)).dt.strftime(date_format).values
 
 @register_function(scope='dt')
 def dt_floor(x, freq, *args):
