77eb3fdb67805e763d156675f1de436491209ab8
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2020-06-27 22:01:50 +0200

    feat(core): allow vaex.from_pandas to recognize pd dtype "string"

--- tests/repr_test.py
+++ tests/repr_test.py
@@ -67,28 +67,3 @@ def test_slice_filtered_remte(ds_remote):
     df = ds_remote
     dff = df[df.x > 0]
     assert "0.0bla" not in repr(dff[['x']])
-
-
-def test_repr_from_pandas():
-    dd_dict = {
-        'boolean': [True, True, False, None, True],
-        'text': ['This', 'is', 'some', 'text', 'so...'],
-        'numbers_1': [1, 30, -2, 1.5, 0.000],
-        'numbers_2': [1, None, -2, 1.5, 0.000],
-        'numbers_3': [1, np.nan, -2, 1.5, 0.000],
-        'datetime_1': [pd.NaT, datetime.datetime(2019, 1, 1, 1, 1, 1), datetime.datetime(2019, 1, 1, 1, 1, 1), datetime.datetime(2019, 1, 1, 1, 1, 1), datetime.datetime(2019, 1, 1, 1, 1, 1)],
-        'datetime_2': [pd.NaT, None, pd.NaT, pd.NaT, pd.NaT],
-        'datetime_3': [pd.Timedelta('1M'), pd.Timedelta('1D'), pd.Timedelta('100M'), pd.Timedelta('2D'), pd.Timedelta('1H')],
-        'datetime_4': [pd.Timestamp('2001-1-1 2:2:11'), pd.Timestamp('2001-12'), pd.Timestamp('2001-10-1'), pd.Timestamp('2001-03-1 2:2:11'), pd.Timestamp('2001-1-1 2:2:11')],
-        'datetime_5': [datetime.date(2010, 1, 1), datetime.date(2010, 1, 1), datetime.date(2010, 1, 1), datetime.date(2010, 1, 1), datetime.date(2010, 1, 1)],
-        'datetime_6': [datetime.time(21, 1, 1), datetime.time(21, 1, 1), datetime.time(21, 1, 1), datetime.time(21, 1, 1), datetime.time(21, 1, 1)],
-    }
-
-    # Get pandas dataframe
-    dd = pd.DataFrame(dd_dict)
-    dd['datetime_7'] = pd.to_timedelta(dd['datetime_2'] - dd['datetime_1'])
-    ds = vaex.from_pandas(dd)
-    repr_value = repr(ds)
-    str_value = str(ds)
-    assert 'NaT' in repr_value
-    assert 'NaT' in str_value
