df0f9fd623f6283c2ec25729fc5323e5510d273b
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-07-17 17:08:15 +0200

    tests: skip thread test on windows (seems to hang travis often)

--- ci/02-create-vaex-dev-env.sh
+++ ci/02-create-vaex-dev-env.sh
@@ -4,8 +4,8 @@ PYTHON_VERSION=${1:-3.7}
 CONDA=${2:-conda}
 conda config --set always_yes yes --set changeps1 no
 $CONDA update -y -q -c conda-forge $CONDA
-$CONDA create -y -q -n vaex-dev python=$PYTHON_VERSION
+$CONDA create -y -q -c conda-forge -n vaex-dev python=$PYTHON_VERSION
 source activate vaex-dev
 $CONDA install -y -q --file ci/conda-env-nightlies.yml -c arrow-nightlies -c conda-forge
-$CONDA install -y -q --file ci/conda-env.yml --file ci/conda-env-notebooks.yml -c conda-forge
+$CONDA install -y -q compilers --file ci/conda-env.yml --file ci/conda-env-notebooks.yml -c conda-forge
 $CONDA init bash
