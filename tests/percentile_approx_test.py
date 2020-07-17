e6a613b5061cca89d7ca111df6231a81ce1fb490
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-07-17 13:26:19 +0200

    chore(test): skip percentile issue

--- tests/percentile_approx_test.py
+++ tests/percentile_approx_test.py
@@ -7,10 +7,7 @@ import sys
 
 version = tuple(map(int, np.__version__.split('.')))
 
-@pytest.mark.skipif(((1,17,0) <= version <= (1,18,1)) and platform.system().lower() == 'windows', reason="strange ref count issue with numpy")
-@pytest.mark.skipif(((1,17,0) <= version <= (1,18,5)) and platform.system().lower() == 'linux' and sys.version_info[:2] == (3,6), reason="strange ref count issue with numpy")
-@pytest.mark.skipif(((1,17,0) <= version <= (1,18,5)) and platform.system().lower() == 'linux' and sys.version_info[:2] == (3,7), reason="strange ref count issue with numpy")
-@pytest.mark.skipif(((1,17,0) <= version <= (1,18,5)) and platform.system().lower() == 'linux' and sys.version_info[:2] == (3,8), reason="strange ref count issue with numpy")
+@pytest.mark.skipif(platform.system().lower() != 'darwin', reason="strange ref count issue with numpy")
 def test_percentile_approx():
     df = vaex.example()
     # Simple test
@@ -29,10 +26,7 @@ def test_percentile_approx():
     np.testing.assert_array_almost_equal(percentiles_2d, expected_result, decimal=1)
 
 
-@pytest.mark.skipif(((1,17,0) <= version <= (1,18,1)) and platform.system().lower() == 'windows', reason="strange ref count issue with numpy")
-@pytest.mark.skipif(((1,17,0) <= version <= (1,18,5)) and platform.system().lower() == 'linux' and sys.version_info[:2] == (3,6), reason="strange ref count issue with numpy")
-@pytest.mark.skipif(((1,17,0) <= version <= (1,18,5)) and platform.system().lower() == 'linux' and sys.version_info[:2] == (3,7), reason="strange ref count issue with numpy")
-@pytest.mark.skipif(((1,17,0) <= version <= (1,18,5)) and platform.system().lower() == 'linux' and sys.version_info[:2] == (3,8), reason="strange ref count issue with numpy")
+@pytest.mark.skipif(platform.system().lower() != 'darwin', reason="strange ref count issue with numpy")
 def test_percentile_1d():
     x = np.array([0, 0, 10, 100, 200])
     df = vaex.from_arrays(x=x)
