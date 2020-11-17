0460d73ebb3b06175851e4be7ac24f45b9e2a043
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-11-17 12:33:47 +0100

    only strip on linux

--- ci/03-install-vaex.sh
+++ ci/03-install-vaex.sh
@@ -3,7 +3,7 @@ set -e
 conda config --set always_yes yes --set changeps1 no
 source activate vaex-dev
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    export CFLAGS='-Wl,-strip-all';
-    export CXXFLAGS='-Wl,-strip-all';
+    export CFLAGS='-Wl,-strip-all'
+    export CXXFLAGS='-Wl,-strip-all'
 fi
 pip install -e .
