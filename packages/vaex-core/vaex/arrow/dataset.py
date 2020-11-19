86c6c89c41a3cd5524e440683c603dea015da3c2
Laurențiu Nicola <lnicola@users.noreply.github.com>
2020-11-19 19:55:42 +0200

    fix: properly handle Apache Arrow files and stream (#809)
    We didn't close the writer, leading to invalid files, and we now
    detect if a file is written as stream or file instead of trail and
    error.
    
    Co-authored-by: Laurențiu Nicola <lnicola@dend.ro>
    
    Co-authored-by: Maarten A. Breddels <maartenbreddels@gmail.com>

--- packages/vaex-core/vaex/arrow/dataset.py
+++ packages/vaex-core/vaex/arrow/dataset.py
@@ -154,16 +154,16 @@ def from_table(table):
 
 def open(path, fs_options):
     source = vaex.file.open_for_arrow(path=path, mode='rb', fs_options=fs_options, mmap=True)
-    try:
-        # first we try if it opens as stream
-        reader = pa.ipc.open_stream(source)
-    except pa.lib.ArrowInvalid:
-        # if not, we open as file
+    file_signature = source.read(6)
+    is_arrow_file = file_signature == b'ARROW1'
+    source.seek(0)
+
+    if is_arrow_file:
         reader = pa.ipc.open_file(source)
         # for some reason this reader is not iterable
         batches = [reader.get_batch(i) for i in range(reader.num_record_batches)]
     else:
-        # if a stream, we're good
+        reader = pa.ipc.open_stream(source)
         batches = reader  # this reader is iterable
     table = pa.Table.from_batches(batches)
     return from_table(table)
