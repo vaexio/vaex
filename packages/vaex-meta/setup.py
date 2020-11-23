10ce5f9fdf07f91f728e18da8a88b510b669be21
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-11-23 20:16:27 +0100

    Release 4.0.0-alpha.7 of vaex-core

--- packages/vaex-meta/setup.py
+++ packages/vaex-meta/setup.py
@@ -16,7 +16,7 @@ version = version.__version__
 url = 'https://www.github.com/maartenbreddels/vaex'
 
 install_requires = [
-      'vaex-core>=4.0.0-alpha.6,<5',
+      'vaex-core>=4.0.0-alpha.7,<5',
       'vaex-viz>=0.5.0-dev.1,<0.6',
       'vaex-server>=0.4.0-dev.1,<0.5',
       'vaex-hdf5>=0.7.0-alpha.3,<0.8',
