0acfb8d0832d7a6479718bcb863bd97714b9ce35
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-11-23 19:42:03 +0100

    Release 0.6.0-dev.1 of vaex-jupyter

--- packages/vaex-meta/setup.py
+++ packages/vaex-meta/setup.py
@@ -21,7 +21,7 @@ install_requires = [
       'vaex-server>=0.4.0-dev.1,<0.5',
       'vaex-hdf5>=0.7.0-alpha.3,<0.8',
       'vaex-astro>=0.8.0-dev.1,<0.9',
-      'vaex-jupyter>=0.6.0-dev.0,<0.7',
+      'vaex-jupyter>=0.6.0-dev.1,<0.7',
       'vaex-ml>=0.11.0-alpha.4,<0.12',
       # vaex-graphql is not on conda-forge yet
 ]
