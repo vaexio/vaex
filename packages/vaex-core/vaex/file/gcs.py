0c54dd8f57f51a31be9b5fce8be66cb3641773cb
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-11-24 11:33:37 +0100

    fix: gcs options were ignored

--- packages/vaex-core/vaex/file/gcs.py
+++ packages/vaex-core/vaex/file/gcs.py
@@ -22,11 +22,11 @@ def glob(path, fs_options={}):
 
 def parse(path, fs_options={}, for_arrow=False):
     path = path.replace('fsspec+gs://', 'gs://')
-    path, options = split_options(path, fs_options)
+    path, fs_options = split_options(path, fs_options)
     scheme, path = split_scheme(path)
     assert scheme == 'gs'
     use_cache = fs_options.pop('cache', 'true') in [True, 'true', 'True', '1']
-    fs = gcsfs.GCSFileSystem(**options,)
+    fs = gcsfs.GCSFileSystem(**fs_options)
     fs = pyarrow.fs.FSSpecHandler(fs)
     if use_cache:
         fs = FileSystemHandlerCached(fs, scheme='gs', for_arrow=for_arrow)
