833d9d02234ee05fb0add77e70c9aeaa7d0bb0cd
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2021-02-02 13:47:52 +0100

    Improve the test

--- tests/from_pandas_test.py
+++ tests/from_pandas_test.py
@@ -11,6 +11,7 @@ def test_from_pandas():
         'text_missing': pd.Series(['Some', 'parts', None, 'missing', None], dtype='string'),
         'float': [1, 30, -2, 1.5, 0.000],
         'float_missing': [1, None, -2, 1.5, 0.000],
+        'float_missing_masked': pd.Series([1, None, -2, 1.5, 0.000], dtype=pd.Float64Dtype()),
         'int_missing': pd.Series([1, None, 5, 1, 10], dtype='Int64'),
         'datetime_1': [pd.NaT, datetime.datetime(2019, 1, 1, 1, 1, 1), datetime.datetime(2019, 1, 1, 1, 1, 1), datetime.datetime(2019, 1, 1, 1, 1, 1), datetime.datetime(2019, 1, 1, 1, 1, 1)],
         'datetime_2': [pd.NaT, None, pd.NaT, pd.NaT, pd.NaT],
@@ -22,7 +23,6 @@ def test_from_pandas():
 
     # Get pandas dataframe
     pandas_df = pd.DataFrame(dd_dict)
-    pandas_df['float_missing_masked'] = pandas_df['float_missing'].astype(pd.Float64Dtype())
     pandas_df['datetime_7'] = pd.to_timedelta(pandas_df['datetime_2'] - pandas_df['datetime_1'])
     vaex_df = vaex.from_pandas(pandas_df)
     repr_value = repr(vaex_df)
