d5805b2ea501146351a26ad27e2df7d95bf67464
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-03-09 11:15:46 +0100

    Release 0.11.0 of vaex-ml

--- packages/vaex-ml/setup.py
+++ packages/vaex-ml/setup.py
@@ -12,7 +12,7 @@ author_email= 'jovan.veljanoski@gmail.com'
 license     = 'MIT'
 version     = version.__version__
 url         = 'https://www.github.com/vaexio/vaex'
-install_requires_ml = ['vaex-core>=4.0.0-alpha.3,<5', 'numba', 'traitlets', 'jinja2']
+install_requires_ml = ['vaex-core>=4.0.0,<5', 'numba', 'traitlets', 'jinja2']
 
 setup(name=name + '-ml',
       version=version,
