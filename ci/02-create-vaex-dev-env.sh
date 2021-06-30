8356d298e757d251531b9b0a73808de1c20b2d34
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-06-30 10:38:28 +0200

    💚 use conda-incubator/setup-miniconda@v2

--- ci/02-create-vaex-dev-env.sh
+++ ci/02-create-vaex-dev-env.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -x -e
+
+if [ -f ${HOME}/.bashrc ]; then
+    source ${HOME}/.bashrc
+else
+    source ${HOME}/.bash_profile
+fi
+
 PYTHON_VERSION=${1:-3.7}
 CONDA=${2:-conda}
 conda config --set always_yes yes --set changeps1 no
@@ -7,7 +14,6 @@ conda config --remove channels defaults
 conda config --add channels msys2
 $CONDA update -y -q -c conda-forge $CONDA
 $CONDA create -y -q -c conda-forge -n vaex-dev python=$PYTHON_VERSION
-source activate vaex-dev
+conda activate vaex-dev
 # $CONDA install -y -q --file ci/conda-env-nightlies.yml -c arrow-nightlies -c conda-forge
 $CONDA install -y -q compilers --file ci/conda-env.yml --file ci/conda-env-notebooks.yml -c conda-forge -c numba/label/dev
-$CONDA init bash
