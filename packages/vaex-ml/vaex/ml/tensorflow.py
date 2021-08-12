ecf300e604ae848cdd3c9685f78d3aa19c7a77af
Jovan Veljanoski <jovan.veljanoski@gmail.com>
2021-08-13 01:33:05 +0200

    feat(ml): generator for streaming vaex data to keras model, and improved testing

--- packages/vaex-ml/vaex/ml/tensorflow.py
+++ packages/vaex-ml/vaex/ml/tensorflow.py
@@ -13,6 +13,102 @@ import vaex.ml.state
 import vaex.ml.generate
 
 
+class DataFrameAccessorTensorflow():
+    def __init__(self, ml):
+        self.ml = ml
+        self.df = self.ml.df
+
+    def to_keras_generator(self, features, target=None, batch_size=1024, parallel=True, shuffle=True, infinite=True, verbose=True):
+        """Return a batch generator suitable as a Keras datasource.  Note that by default the generator is infinite,
+        i.e. it loops continuously ovder the data. Thus you need to specify the "steps_per_epoch" arg when fitting a Keras model,
+        the "validation_steps" when using it for validation, and "steps" when calling the "predict" method of a keras model.
+
+        :param features: A list of features.
+        :param target: The dependent or target column or a list of columns, if any.
+        :param batch_size: Number of samples per chunk of data. This can be thought of as the batch size.
+        :param parallel: If True, vaex will evaluate the data chunks in parallel.
+        :param shuffle: If True, shuffle the data before every pass.
+        :param infinite: If True, the generator is infinite, i.e. it loops continously over the data. If False, the generator does only one pass over the data.
+        :parallel verbose: If True, show an info on the recommended "steps_per_epoch" based on the total number of samples and "batch_size".
+
+        Example:
+
+        >>> import vaex
+        >>> import vaex.ml
+        >>> import tensorflow.keras as K
+
+        >>> df = vaex.example()
+        >>> features = ['x', 'y', 'z', 'vx', 'vz', 'vz']
+        >>> target = 'FeH'
+
+        >>> # Scaling the features
+        >>> df = df.ml.minmax_scaler(features=features)
+        >>> features = df.get_column_names(regex='^minmax_')
+
+        >>> # Create a training generator
+        >>> train_generator = df.ml.tensorflow.to_keras_generator(features=features, target=target, batch_size=512)
+        Recommended "steps_per_epoch" arg: 645.0
+
+        >>> # Build a neural network model
+        >>> nn_model = K.Sequential()
+        >>> nn_model.add(K.layers.Dense(4, activation='tanh'))
+        >>> nn_model.add(K.layers.Dense(4, activation='tanh'))
+        >>> nn_model.add(K.layers.Dense(1, activation='linear'))
+        >>> nn_model.compile(optimizer='sgd', loss='mse')
+
+        >>> nn_model.fit(x=train_generator, epochs=3, steps_per_epoch=645)
+        Epoch 1/3
+        645/645 [==============================] - 3s 5ms/step - loss: 0.2068
+        Epoch 2/3
+        645/645 [==============================] - 3s 5ms/step - loss: 0.1706
+        Epoch 3/3
+        645/645 [==============================] - 3s 5ms/step - loss: 0.1705
+        """
+
+        if verbose:
+            steps_per_epoch = np.ceil(len(self.df) / batch_size)
+            print(f'Recommended "steps_per_epoch" arg: {steps_per_epoch}')
+
+        def _generator(features, target, chunk_size, parallel, shuffle, infinite):
+            if shuffle:
+                df = self.df.shuffle().copy()
+            else:
+                df = self.df.copy()
+
+            if target is not None:
+                target = vaex.utils._ensure_list(target)
+                target = vaex.utils._ensure_strings_from_expressions(target)
+                n_target_cols = len(target)
+                column_names = features + target
+            else:
+                column_names = features
+
+            while True:
+                if shuffle:
+                    df = self.df.shuffle().copy()
+                else:
+                    df = self.df.copy()
+
+                if target is not None:
+                    for i1, i2, chunk, in df.evaluate_iterator(column_names, chunk_size=chunk_size, parallel=parallel, array_type='numpy'):
+                        chunk_shape = len(chunk[0].shape) + 1
+                        transpose_order = np.arange(1, chunk_shape).tolist() + [0]
+                        X = np.array(chunk[:-n_target_cols]).transpose(transpose_order)
+                        y = np.array(chunk[-n_target_cols:], copy=False).T
+                        yield (X, y)
+
+                else:
+                    for i1, i2, chunk, in df.evaluate_iterator(column_names, chunk_size=chunk_size, parallel=parallel, array_type='numpy'):
+                        chunk_shape = len(chunk[0].shape) + 1
+                        transpose_order = np.arange(1, chunk_shape).tolist() + [0]
+                        X = np.array(chunk).transpose(transpose_order)
+                        yield (X, )
+
+                if not infinite:
+                    break
+
+        return _generator(features=features, target=target, chunk_size=batch_size, parallel=parallel, shuffle=shuffle, infinite=infinite)
+
 
 @vaex.serialize.register
 @vaex.ml.generate.register
--- tests/ml/tensorflow_test.py
+++ tests/ml/tensorflow_test.py
@@ -1,4 +1,3 @@
-import sys
 import pytest
 pytest.importorskip("tensorflow")
 
@@ -9,35 +8,31 @@ import vaex.ml
 import vaex.ml.tensorflow
 
 
-def test_keras_model(tmpdir, df_iris):
+def test_keras_model_classification(tmpdir, df_iris):
     df = df_iris
     copy = df.copy()
     features = ['sepal_width', 'petal_length', 'sepal_length', 'petal_width']
     target = 'class_'
 
-    def create_neural_network():
-        inp = K.layers.Input(shape=(4,), name='input')
-        x = K.layers.Dense(4, activation='relu', name='layer')(inp)
-        x = K.layers.Dense(3, activation='softmax', name='output')(x)
+    # Preprocessing
+    df = df.ml.minmax_scaler(features)
+    df = df.ml.one_hot_encoder(features=[target])
+    features = df.get_column_names(regex='^minmax')
+    targets = df.get_column_names(regex='^class__')
 
-        nn = K.models.Model(inputs=inp, outputs=x)
-
-        nn.compile(optimizer=K.optimizers.RMSprop(learning_rate=0.01),
-                loss=K.losses.categorical_crossentropy,
-                metrics='accuracy')
-
-        return nn
-
-    nn = create_neural_network()
+    # Create the neural netwrok model
+    nn_model = K.Sequential()
+    nn_model.add(K.layers.Dense(4, activation='tanh'))
+    nn_model.add(K.layers.Dense(3, activation='softmax'))
+    nn_model.compile(optimizer=K.optimizers.RMSprop(learning_rate=0.01),
+                    loss=K.losses.categorical_crossentropy,
+                    metrics='accuracy')
 
     X = df[features].values
-    y = df[target].values
-    y_enc = K.utils.to_categorical(y)
-    nn.fit(x=X, y=y_enc, validation_split=0.05, epochs=11)
+    y = df[targets].values
+    nn_model.fit(x=X, y=y, validation_split=0.05, epochs=11)
 
-    keras_model = vaex.ml.tensorflow.KerasModel(features=features,
-                                                prediction_name='pred',
-                                                model=nn)
+    keras_model = vaex.ml.tensorflow.KerasModel(features=features, prediction_name='pred', model=nn_model)
     df_trans = keras_model.transform(df)
     assert df_trans.pred.shape == (150, 3)
 
@@ -46,3 +41,63 @@ def test_keras_model(tmpdir, df_iris):
     copy.state_load(state_path)
 
     assert copy.pred.shape == (150, 3)
+
+
+def test_keras_model_regression(df_example):
+    df = df_example
+    df_train, df_valid, df_test = df.split_random([0.8, 0.1, 0.1], random_state=42)
+    features = ['vx', 'vy', 'vz']
+    target = 'FeH'
+
+    # Scaling the features
+    df_train = df_train.ml.minmax_scaler(features=features)
+    features = df_train.get_column_names(regex='^minmax_')
+
+    # Apply preprocessing to the validation
+    state_prep = df_train.state_get()
+    df_valid.state_set(state_prep)
+
+    train_gen = df_train.ml.tensorflow.to_keras_generator(features=features, target=target, batch_size=512)
+    valid_gen = df_valid.ml.tensorflow.to_keras_generator(features=features, target=target, batch_size=512)
+
+    # Create the model
+    nn_model = K.Sequential()
+    nn_model.add(K.layers.Dense(3, activation='tanh'))
+    nn_model.add(K.layers.Dense(1, activation='linear'))
+    nn_model.compile(optimizer='sgd', loss='mse')
+    nn_model.fit(x=train_gen, validation_data=valid_gen, epochs=3, steps_per_epoch=516, validation_steps=64)
+
+    keras_model = vaex.ml.tensorflow.KerasModel(features=features, prediction_name='pred', model=nn_model)
+    df_train = keras_model.transform(df_train)
+
+    # The final state transfer
+    state_final = df_train.state_get()
+    df_valid.state_set(state_final)
+    df_test.state_set(state_final)
+
+    assert 'pred' in df_train
+    assert 'pred' in df_valid
+    assert 'pred' in df_test
+    assert df_valid.shape == (33000, 15)
+    assert df_test.shape == (33000, 15)
+
+
+@pytest.mark.parametrize("parallel", [False, True])
+def test_to_keras_generator(df_example, parallel):
+    df = df_example
+    df = df[:1000]
+    features = ['x', 'y', 'z', 'vx', 'vy', 'vx']
+    target = 'FeH'
+
+    train_gen_1 = df.ml.tensorflow.to_keras_generator(features=features, target=target, parallel=parallel, batch_size=100, shuffle=False)
+    train_gen_2 = df.ml.tensorflow.to_keras_generator(features=features, target=target, parallel=parallel, batch_size=100, shuffle=False)
+
+    for i in range(30):
+        batch_1 = next(train_gen_1)
+        assert len(batch_1) == 2
+        assert batch_1[0].shape == (100, 6)
+        assert batch_1[1].shape == (100, 1)
+        if i > 9:
+            batch_2 = next(train_gen_2)
+            assert batch_1[0].tolist() == batch_2[0].tolist()
+            assert batch_1[1].tolist() == batch_2[1].tolist()
