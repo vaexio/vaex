e1795b60304e6e133dc85350b0b2a663fa7ed395
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-08-13 16:43:01 +0200

    🔖 vaex-ml 0.13.0 released

--- packages/vaex-meta/setup.py
+++ packages/vaex-meta/setup.py
@@ -22,7 +22,7 @@ install_requires = [
       'vaex-hdf5>=0.9.0,<0.10',
       'vaex-astro>=0.8.3,<0.9',
       'vaex-jupyter>=0.6.0,<0.7',
-      'vaex-ml>=0.12.0,<0.13',
+      'vaex-ml>=0.13.0,<0.13',
       # vaex-graphql is not on conda-forge yet
 ]
 
