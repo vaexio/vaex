ef819e08fa8a8ca8500382497772f97c2d398f10
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-09-02 09:38:34 +0200

    Verbose test output (catch where segfault is)

--- ci/04-run-test-suite.sh
+++ ci/04-run-test-suite.sh
@@ -8,4 +8,4 @@ else
 fi
 conda activate vaex-dev
 export VAEX_SERVER_OVERRIDE='{"dataframe.vaex.io":"dataframe-dev.vaex.io"}'
-py.test tests packages/vaex-core/vaex/datatype_test.py packages/vaex-core/vaex/file/ packages/vaex-core/vaex/test/dataset.py::TestDataset --doctest-modules packages/vaex-core/vaex/datatype.py packages/vaex-core/vaex/utils.py --timeout=1000
+py.test tests -v packages/vaex-core/vaex/datatype_test.py packages/vaex-core/vaex/file/ packages/vaex-core/vaex/test/dataset.py::TestDataset --doctest-modules packages/vaex-core/vaex/datatype.py packages/vaex-core/vaex/utils.py --timeout=1000
