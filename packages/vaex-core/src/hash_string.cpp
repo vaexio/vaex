39f2a3ffcb98d7e35c762197580aa8c7793e224f
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-09-08 08:52:04 +0200

    fix mscv 14.0

--- packages/vaex-core/src/hash_string.cpp
+++ packages/vaex-core/src/hash_string.cpp
@@ -5,47 +5,47 @@
 namespace vaex {
 
 // cannot get stringview to work with msvc
-struct equal_string {
-    using is_transparent = void;
+// struct equal_string {
+//     using is_transparent = void;
 
-    bool operator()(const string &str, const string_view &strview) const { return str == strview; }
+//     bool operator()(const string &str, const string_view &strview) const { return str == strview; }
 
-    bool operator()(const string_view &strview1, const string_view &strview2) const { return strview1 == strview2; }
+//     bool operator()(const string_view &strview1, const string_view &strview2) const { return strview1 == strview2; }
 
-    bool operator()(const string_view strview, const string &str) const { return strview == str; }
+//     bool operator()(const string_view strview, const string &str) const { return strview == str; }
 
-    bool operator()(const string &str1, const string &str2) const { return str1 == str2; }
-};
+//     bool operator()(const string &str1, const string &str2) const { return str1 == str2; }
+// };
 
-struct hash_string {
-    std::size_t operator()(const string &str) const {
-#ifdef VAEX_HASH_STRING_SIMPLE
-        unsigned int hash = 1;
-        const char *s = str.data();
-        const char *end = s + str.size();
-        while (s != end) {
-            hash = hash * 101 + *s++;
-        }
-        return hash;
-#else
-        return std::hash<string>()(str);
-#endif
-    }
+// struct hash_string {
+//     std::size_t operator()(const string &str) const {
+// #ifdef VAEX_HASH_STRING_SIMPLE
+//         unsigned int hash = 1;
+//         const char *s = str.data();
+//         const char *end = s + str.size();
+//         while (s != end) {
+//             hash = hash * 101 + *s++;
+//         }
+//         return hash;
+// #else
+//         return std::hash<string>()(str);
+// #endif
+//     }
 
-    std::size_t operator()(const string_view str_view) const {
-#ifdef VAEX_HASH_STRING_SIMPLE
-        unsigned int hash = 1;
-        const char *s = str_view.data();
-        const char *end = s + str_view.size();
-        while (s != end) {
-            hash = hash * 101 + *s++;
-        }
-        return hash;
-#else
-        return std::hash<string_view>()(str_view);
-#endif
-    }
-};
+//     std::size_t operator()(const string_view str_view) const {
+// #ifdef VAEX_HASH_STRING_SIMPLE
+//         unsigned int hash = 1;
+//         const char *s = str_view.data();
+//         const char *end = s + str_view.size();
+//         while (s != end) {
+//             hash = hash * 101 + *s++;
+//         }
+//         return hash;
+// #else
+//         return std::hash<string_view>()(str_view);
+// #endif
+//     }
+// };
 
 template <class Type, class Cls>
 void bind_common(Cls &cls) {
@@ -71,10 +71,7 @@ void init_hash_string(py::module &m) {
     {
         typedef counter<> Type;
         std::string countername = "counter_string";
-        auto cls = py::class_<Type>(m, countername.c_str())
-            .def(py::init<int>())
-            .def("counts", &Type::counts)
-            ;
+        auto cls = py::class_<Type>(m, countername.c_str()).def(py::init<int>()).def("counts", &Type::counts);
         bind_common<Type>(cls);
     }
     {
