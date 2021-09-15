vaexio/vaex

commit 76626edcb8a86a518a11a53ab8315d0d195f5fd9

🐎 refactor hashmaps, no merge phase, faster serialization (#1525)
* refactor hashmaps to not have to be merged and faster serialization

packages/vaex-core/src/hash.hpp
@@ -1,9 +1,25 @@
 // #include "flat_hash_map.hpp"
 // #include "unordered_map.hpp"
-#include "tsl/hopscotch_set.h"
+// #define VAEX_USE_TSL 1
+// #define VAEX_USE_ABSL 1
+
+#ifdef VAEX_USE_TSL
 #include "tsl/hopscotch_map.h"
+#include "tsl/hopscotch_set.h"
+#endif
+
+#ifdef VAEX_USE_ABSL
+#include "absl/container/flat_hash_map.h"
+#endif
+
+#include <mutex>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 namespace vaex {
+
+namespace py = pybind11;
 
 // simple and fast https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key
 static inline std::size_t _hash64(uint64_t x) {
@@ -16,30 +32,25 @@ static inline std::size_t _hash64(uint64_t x) {
 // 64 and 32 bit should not have identity as hash function, if the lower bits are not used (like with Gaia's source_id)
 // we get horrible performance, which can be alivated by using prime growth
 
-template<typename T>
+template <typename T>
 struct hash {
-    std::size_t operator()(T const& val) const {
+    std::size_t operator()(T const &val) const {
         std::hash<T> h;
         return h(val);
     }
 };
 
-template<>
+template <>
 struct hash<uint64_t> {
-    std::size_t operator()(uint64_t const val) const {
-        return _hash64(val);
-    }
+    std::size_t operator()(uint64_t const val) const { return _hash64(val); }
 };
 
-template<>
+template <>
 struct hash<int64_t> {
-    std::size_t operator()(uint64_t const val) const {
-        return _hash64(*reinterpret_cast<const int64_t*>(&val));
-    }
+    std::size_t operator()(uint64_t const val) const { return _hash64(*reinterpret_cast<const int64_t *>(&val)); }
 };
 
-
-template<>
+template <>
 struct hash<int32_t> {
     std::size_t operator()(int32_t const val) const {
         uint64_t v(val);
@@ -47,7 +58,7 @@ struct hash<int32_t> {
     }
 };
 
-template<>
+template <>
 struct hash<uint32_t> {
     std::size_t operator()(uint32_t const val) const {
         uint64_t v(val);
@@ -55,36 +66,207 @@ struct hash<uint32_t> {
     }
 };
 
-template<>
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
+template <>
 struct hash<float> {
     std::size_t operator()(float const val) const {
         uint64_t v(0);
-        *reinterpret_cast<float*>(&v) = val;
+        *reinterpret_cast<float *>(&v) = val;
         return _hash64(v);
     }
 };
 
-template<>
+template <>
 struct hash<double> {
-    std::size_t operator()(double const val) const {
-        return _hash64(*reinterpret_cast<const uint64_t*>(&val));
-    }
+    std::size_t operator()(double const val) const { return _hash64(*reinterpret_cast<const uint64_t *>(&val)); }
 };
+#pragma GCC diagnostic pop
 
-
-template<class Key, class Value, class Hash=vaex::hash<Key>, class Compare=std::equal_to<Key>>
-// using hashmap = ska::flat_hash_map<Key, Value, Hash, Compare>;
+#ifdef VAEX_USE_TSL
+template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
 using hashmap = tsl::hopscotch_map<Key, Value, Hash, Compare>;
-template<class Key, class Value, class Hash=vaex::hash<Key>, class Compare=std::equal_to<Key>>
+template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
 using hashmap_pg = tsl::hopscotch_pg_map<Key, Value, Hash, Compare>;
+
+template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+using hashmap_store = tsl::hopscotch_map<Key, Value, Hash, Compare, std::allocator<std::pair<Key, Value>>, 30, true>;
+template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+using hashmap_pg_store = tsl::hopscotch_pg_map<Key, Value, Hash, Compare, std::allocator<std::pair<Key, Value>>, 30, true>;
+#endif
+
+#ifdef VAEX_USE_ABSL
+template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+using hashmap = absl::flat_hash_map<Key, Value, Hash, Compare>;
+template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+using hashmap_pg = absl::flat_hash_map<Key, Value, Hash, Compare>;
+
+template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+using hashmap_store = absl::flat_hash_map<Key, Value, Hash, Compare>;
+template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+using hashmap_pg_store = absl::flat_hash_map<Key, Value, Hash, Compare>;
+#endif
+
 // template<class Key,  class Hash, class Compare>
 // using hashset = tsl::hopscotch_set<Key, Hash, Compare>;
 
 // we cannot modify .second, instead use .value()
 // see https://github.com/Tessil/hopscotch-map
-template<class I, class V>
-inline void set_second(I& it, V &&value) {
+template <class I, class V>
+inline void set_second(I &it, V &&value) {
+#ifdef VAEX_USE_TSL
     it.value() = value;
+#else
+    it->second = value;
+#endif
 }
 
-}
+template <class Derived, class KeyType, class Hashmap = hashmap<KeyType, int64_t>>
+class hash_common {
+  public:
+    using value_type = int64_t;
+    using key_type = KeyType;
+    using hashmap_type = Hashmap;
+    using hasher = typename hashmap_type::hasher;
+    using hasher_map = typename hashmap_type::hasher;
+    using hasher_map_choice = typename hashmap_type::hasher; // typename vaex::hash<key_type>;
+
+    hash_common(int16_t nmaps) : maps(nmaps), maplocks(nmaps), nan_count(0), null_count(0), sealed(false) {}
+
+    void update1(key_type &key) {
+        std::size_t hash = hasher_map_choice()(key);
+        size_t map_index = (hash % this->maps.size());
+        update1(map_index, key);
+    }
+
+    void update1(uint16_t map_index, key_type &key, int64_t index = 0) {
+        auto &map = this->maps[map_index];
+        auto search = map.find(key);
+        auto end = map.end();
+        if (search == end) {
+            static_cast<Derived &>(*this).add_new(map_index, key, index);
+        } else {
+            static_cast<Derived &>(*this).add_existing(search, map_index, key, index);
+        }
+    }
+    value_type update1_null(int64_t index = 0) {
+        this->null_count++;
+        return static_cast<Derived &>(*this).add_null(index);
+    }
+
+    py::object flatten_values(py::array_t<value_type> values, py::array_t<int16_t> map_index, py::array_t<value_type> out) {
+        int64_t size = values.size();
+        if (values.size() != out.size()) {
+            throw std::runtime_error("output array does not match length of values");
+        }
+        if (values.size() != map_index.size()) {
+            throw std::runtime_error("map_index array does not match length of values");
+        }
+        auto result_ptr = out.template mutable_unchecked<1>();
+        auto out_ptr = values.template unchecked<1>();
+        auto map_index_ptr = map_index.template unchecked<1>();
+        std::vector<int64_t> offsets = this->offsets();
+        {
+            py::gil_scoped_release gil;
+            for (int64_t i = 0; i < size; i++) {
+                result_ptr(i) = out_ptr(i) + offsets[map_index_ptr(i)];
+            }
+        }
+        return std::move(out);
+    }
+
+    virtual value_type nan_index() { return 0; }
+    virtual value_type null_index() { return this->nan_count ? 1 : 0; }
+    py::list keys() {
+        // if(!this->sealed) {
+        //     throw std::runtime_error("hashmap not sealed, call .seal() first");
+        // }
+        py::list l(this->length());
+        size_t map_index = 0;
+        int64_t natural_order = 0;
+        auto offsets = this->offsets();
+        for (auto &map : this->maps) {
+            for (auto &el : map) {
+                key_type key = el.first;
+                int64_t index = static_cast<Derived &>(*this).key_offset(natural_order++, map_index, el, offsets[map_index]);
+                l[index] = key;
+            }
+            map_index += 1;
+        }
+        if (this->nan_count) {
+            py::object math = py::module::import("math");
+            l[nan_index()] = math.attr("nan");
+        }
+        if (this->null_count) {
+            l[null_index()] = py::none();
+        }
+        return l;
+    }
+
+    void seal() { sealed = true; }
+
+    int64_t count() const {
+        int64_t c = 0;
+        for (size_t i = 0; i < this->maps.size(); i++) {
+            c += this->maps[i].size();
+            if (i == 0) {
+                if (this->null_count) {
+                    c++;
+                }
+                if (this->nan_count) {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+
+    std::vector<int64_t> offsets() const {
+        std::vector<int64_t> v;
+        int64_t offset = 0;
+        for (size_t i = 0; i < this->maps.size(); i++) {
+            v.push_back(offset);
+            offset += this->maps[i].size();
+            if (i == 0) {
+                if (this->null_count) {
+                    offset++;
+                }
+                if (this->nan_count) {
+                    offset++;
+                }
+            }
+        }
+        return v;
+    }
+
+    int64_t offset() const {
+        // null and nan are special cases, and always in the begin
+        return (this->null_count > 0 ? 1 : 0) + (this->nan_count > 0 ? 1 : 0);
+    }
+    int64_t length() const {
+        // normal count and null and nan
+        return count();
+    }
+
+    std::vector<hashmap_type> maps;
+    std::vector<std::mutex> maplocks;
+    int64_t nan_count;
+    int64_t null_count;
+    bool sealed;
+};
+
+template <class K, class V, class Derived>
+class counter_mixin {
+  public:
+    using value_type = V;
+    using key_type = K;
+
+    // ideally, we put common code in mixin classes using CRTP, but this cannot be resolved?
+    // value_type add(int16_t map_index, key_type& value, int64_t index) {
+    //     auto & map = static_cast<Derived&>(*this)->maps[map_index];
+    //     map.emplace(value, 1);
+    //     return 1;
+    // }
+};
+
+} // namespace vaex

packages/vaex-core/src/superutils.cpp
@@ -1,46 +1,45 @@
-#include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#include <numpy/arrayobject.h>
-#include <Python.h>
 #include "hash.hpp"
+#include <Python.h>
+#include <numpy/arrayobject.h>
 
 namespace py = pybind11;
 
-#define custom_isnan(value) (!(value==value))
+#define custom_isnan(value) (!(value == value))
 
 namespace vaex {
-    void init_hash_primitives_power_of_two(py::module &);
-    void init_hash_primitives_prime(py::module &);
-    void init_hash_string(py::module &);
-    void init_hash_object(py::module &);
-}
+void init_hash_primitives_power_of_two(py::module &);
+void init_hash_primitives_prime(py::module &);
+void init_hash_string(py::module &);
+void init_hash_object(py::module &);
+} // namespace vaex
 
 class Mask {
-public:
+  public:
     Mask(size_t length) : length(length), _owns_data(true) {
         mask_data = new uint8_t[length];
         reset();
     }
-    Mask(uint8_t* mask_data, size_t length) : mask_data(mask_data), length(length), _owns_data(false) {
-    }
+    Mask(uint8_t *mask_data, size_t length) : mask_data(mask_data), length(length), _owns_data(false) {}
     virtual ~Mask() {
-        if(_owns_data)
+        if (_owns_data)
             delete[] mask_data;
     }
     std::pair<int64_t, int64_t> indices(int64_t i1, int64_t i2) {
-        if(i2 < i1) {
+        if (i2 < i1) {
             throw std::runtime_error("end index should be larger or equal to start index");
         }
         int64_t count = 0;
         int64_t start = -1, end = -1;
-        for(int64_t i = 0; i < length; i++) {
-            if(mask_data[i] == 1) {
-                if(count == i1) {
+        for (int64_t i = 0; i < length; i++) {
+            if (mask_data[i] == 1) {
+                if (count == i1) {
                     start = i;
                 }
-                if(count == i2) {
+                if (count == i2) {
                     end = i;
                 }
                 count++;
@@ -54,10 +53,10 @@ public:
         // }
         int64_t counted = 0;
         int64_t start = -1, end = -1;
-        for(int64_t i = 0; i < length; i++) {
-            if(mask_data[i] == 1) {
+        for (int64_t i = 0; i < length; i++) {
+            if (mask_data[i] == 1) {
                 counted++;
-                if(counted == logical_offset) {
+                if (counted == logical_offset) {
                     return i;
                 }
             }
@@ -66,13 +65,13 @@ public:
     }
     void reset() {
         py::gil_scoped_release release;
-        std::fill(mask_data, mask_data+length, 2);
+        std::fill(mask_data, mask_data + length, 2);
     }
     int64_t count() {
         py::gil_scoped_release release;
         int64_t count = 0;
-        for(int64_t i = 0; i < length; i++) {
-            if(mask_data[i] == 1) {
+        for (int64_t i = 0; i < length; i++) {
+            if (mask_data[i] == 1) {
                 count++;
             }
         }
@@ -80,24 +79,24 @@ public:
     }
     int64_t is_dirty() {
         py::gil_scoped_release release;
-        for(int64_t i = 0; i < length; i++) {
-            if(mask_data[i] == 2) {
+        for (int64_t i = 0; i < length; i++) {
+            if (mask_data[i] == 2) {
                 return true;
             }
         }
         return false;
     }
-    Mask* view(int64_t start, int64_t end) {
-        if(end < start) {
+    Mask *view(int64_t start, int64_t end) {
+        if (end < start) {
             throw std::runtime_error("end index should be larger or equal to start index");
         }
-        if(start < 0) {
+        if (start < 0) {
             throw std::runtime_error("start should be >= 0");
         }
-        if(end > length) {
+        if (end > length) {
             throw std::runtime_error("end should be <= length");
         }
-        return new Mask(mask_data+start, end-start);
+        return new Mask(mask_data + start, end - start);
     }
     py::array_t<int64_t> first(int64_t amount) {
         auto ar = py::array_t<int64_t>(amount);
@@ -105,18 +104,18 @@ public:
         int64_t found = 0;
         {
             py::gil_scoped_release release;
-            for(size_t i = 0; i < length; i++) {
-                if(mask_data[i] == 1) {
+            for (size_t i = 0; i < length; i++) {
+                if (mask_data[i] == 1) {
                     ar_unsafe(found++) = i;
                 }
-                if(found == amount) {
+                if (found == amount) {
                     break;
                 }
             }
         }
         auto ar_trimmed = py::array_t<int64_t>(found);
         auto ar_trimmed_unsafe = ar_trimmed.mutable_unchecked<1>();
-        for(size_t i = 0; i < found; i++) {
+        for (int64_t i = 0; i < found; i++) {
             ar_trimmed_unsafe(i) = ar_unsafe(i);
         }
         return ar_trimmed;
@@ -127,28 +126,28 @@ public:
         int64_t found = 0;
         {
             py::gil_scoped_release release;
-            for(int64_t i = length-1; i >= 0; i--) {
-                if(mask_data[i] == 1) {
+            for (int64_t i = length - 1; i >= 0; i--) {
+                if (mask_data[i] == 1) {
                     ar_unsafe(found++) = i;
                 }
-                if(found == amount) {
+                if (found == amount) {
                     break;
                 }
             }
         }
         auto ar_ordered = py::array_t<int64_t>(found);
         auto ar_ordered_unsafe = ar_ordered.mutable_unchecked<1>();
-        for(size_t i = 0; i < found; i++) {
-            ar_ordered_unsafe(i) = ar_unsafe(found-1-i);
+        for (size_t i = 0; i < found; i++) {
+            ar_ordered_unsafe(i) = ar_unsafe(found - 1 - i);
         }
         return ar_ordered;
     }
-    uint8_t* mask_data;
+    uint8_t *mask_data;
     int64_t length;
     bool _owns_data;
 };
 
-template<typename T>
+template <typename T>
 std::size_t hash_func(T v) {
     vaex::hash<T> h;
     return h(v);
@@ -162,29 +161,23 @@ PYBIND11_MODULE(superutils, m) {
     py::class_<Mask>(m, "Mask", py::buffer_protocol())
         .def(py::init<size_t>())
         .def(py::init([](py::buffer mask_array) {
-                py::buffer_info info = mask_array.request();
-                if(info.ndim != 1) {
-                    throw std::runtime_error("Expected a 1d byte buffer");
-                }
-                return new Mask((uint8_t*)info.ptr, info.shape[0]);
-            })
-        )
+            py::buffer_info info = mask_array.request();
+            if (info.ndim != 1) {
+                throw std::runtime_error("Expected a 1d byte buffer");
+            }
+            return new Mask((uint8_t *)info.ptr, info.shape[0]);
+        }))
         .def_buffer([](Mask &mask) -> py::buffer_info {
             std::vector<ssize_t> strides = {1};
             std::vector<ssize_t> shapes = {mask.length};
-            return py::buffer_info(
-                (void*)mask.mask_data,                               /* Pointer to buffer */
-                sizeof(bool),                 /* Size of one scalar */
-                py::format_descriptor<bool>::format(), /* Python struct-style format descriptor */
-                1,                       /* Number of dimensions */
-                shapes,                 /* Buffer dimensions */
-                strides
-            );
+            return py::buffer_info((void *)mask.mask_data,                /* Pointer to buffer */
+                                   sizeof(bool),                          /* Size of one scalar */
+                                   py::format_descriptor<bool>::format(), /* Python struct-style format descriptor */
+                                   1,                                     /* Number of dimensions */
+                                   shapes,                                /* Buffer dimensions */
+                                   strides);
         })
-        .def_property_readonly("length", [](const Mask &mask) {
-                return mask.length;
-            }
-        )
+        .def_property_readonly("length", [](const Mask &mask) { return mask.length; })
         .def("indices", &Mask::indices)
         .def("raw_offset", &Mask::raw_offset)
         .def("count", &Mask::count)
@@ -194,12 +187,12 @@ PYBIND11_MODULE(superutils, m) {
         .def("is_dirty", &Mask::is_dirty)
         .def("view", &Mask::view, py::keep_alive<0, 1>())
         // .def("reduce", &Mask::reduce)
-    ;
+        ;
 
     m.def("hash", hash_func<uint64_t>);
 
     vaex::init_hash_primitives_power_of_two(m);
-    vaex::init_hash_primitives_prime(m);
+    // vaex::init_hash_primitives_prime(m);
     vaex::init_hash_string(m);
     vaex::init_hash_object(m);
 }

commit cc4920fcd77fca950c906ac38c5be98ecee005a5

🐎 groupby performance was hurt for data with many lower bits constants
Similar to #1088 but now for groupby.
This regression was introduced in #1525

packages/vaex-core/src/hash.hpp
@@ -84,26 +84,26 @@ struct hash<double> {
 #pragma GCC diagnostic pop
 
 #ifdef VAEX_USE_TSL
-template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+template <class Key, class Value, class Hash = vaex::hash<Key>, class Compare = std::equal_to<Key>>
 using hashmap = tsl::hopscotch_map<Key, Value, Hash, Compare>;
-template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+template <class Key, class Value, class Hash = vaex::hash<Key>, class Compare = std::equal_to<Key>>
 using hashmap_pg = tsl::hopscotch_pg_map<Key, Value, Hash, Compare>;
 
-template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+template <class Key, class Value, class Hash = vaex::hash<Key>, class Compare = std::equal_to<Key>>
 using hashmap_store = tsl::hopscotch_map<Key, Value, Hash, Compare, std::allocator<std::pair<Key, Value>>, 30, true>;
-template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+template <class Key, class Value, class Hash = vaex::hash<Key>, class Compare = std::equal_to<Key>>
 using hashmap_pg_store = tsl::hopscotch_pg_map<Key, Value, Hash, Compare, std::allocator<std::pair<Key, Value>>, 30, true>;
 #endif
 
 #ifdef VAEX_USE_ABSL
-template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+template <class Key, class Value, class Hash = vaex::hash<Key>, class Compare = std::equal_to<Key>>
 using hashmap = absl::flat_hash_map<Key, Value, Hash, Compare>;
-template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+template <class Key, class Value, class Hash = vaex::hash<Key>, class Compare = std::equal_to<Key>>
 using hashmap_pg = absl::flat_hash_map<Key, Value, Hash, Compare>;
 
-template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+template <class Key, class Value, class Hash = vaex::hash<Key>, class Compare = std::equal_to<Key>>
 using hashmap_store = absl::flat_hash_map<Key, Value, Hash, Compare>;
-template <class Key, class Value, class Hash = std::hash<Key>, class Compare = std::equal_to<Key>>
+template <class Key, class Value, class Hash = vaex::hash<Key>, class Compare = std::equal_to<Key>>
 using hashmap_pg_store = absl::flat_hash_map<Key, Value, Hash, Compare>;
 #endif
 

packages/vaex-core/src/superutils.cpp
@@ -192,7 +192,7 @@ PYBIND11_MODULE(superutils, m) {
     m.def("hash", hash_func<uint64_t>);
 
     vaex::init_hash_primitives_power_of_two(m);
-    // vaex::init_hash_primitives_prime(m);
+    vaex::init_hash_primitives_prime(m);
     vaex::init_hash_string(m);
     vaex::init_hash_object(m);
 }

tests/internal/hash_test.py
@@ -50,19 +50,6 @@ def test_set_bool():
     assert len(keys) == 2
     assert set(keys.tolist()) == {True, False}
 
-def test_set_int_multiple_maps():
-    x = np.arange(5, dtype='i4')
-    set0 = ordered_set_int32(2)
-    set0.update(x)
-    set0.seal()
-    # we're not guaranteed an order due to different hash functions on different
-    # platforms
-    values = {}
-    dicts = set0.extract()
-    for dict in dicts:
-        values.update(dict)
-    assert values == {0: 0, 2: 1, 4:2, 1:0, 3:1}
-
 
 @pytest.mark.parametrize("nan", [False, True])
 @pytest.mark.parametrize("missing", [False, True])