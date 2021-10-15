6733996a142a86dd9bac3a01724e26a40f99dfe6
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-10-15 13:58:53 +0200

    trail and error

--- ci/02-create-vaex-dev-env.sh
+++ ci/02-create-vaex-dev-env.sh
@@ -10,7 +10,7 @@ set -x -e
 PYTHON_VERSION=${1:-3.7}
 CONDA=${2:-conda}
 conda config --set always_yes yes --set changeps1 no
-$CONDA update -y -q -c conda-forge $CONDA
+# $CONDA update -y -q -c conda-forge $CONDA
 $CONDA create -y -q -c conda-forge -n vaex-dev python=$PYTHON_VERSION compilers
 $CONDA activate vaex-dev
 $CONDA env update --file ci/conda-env.yml
