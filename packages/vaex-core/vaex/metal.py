6539045a066314ba115ac8d3beded0d36460ceac
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-11-22 20:21:32 +0100

    better deal with types

--- packages/vaex-core/vaex/metal.py
+++ packages/vaex-core/vaex/metal.py
@@ -1,6 +1,7 @@
 import numpy as np
 import threading
 import logging
+import warnings
 
 import vaex.serialize
 from .expression import FunctionSerializableJit
@@ -33,11 +34,16 @@ class FunctionSerializableMetal(FunctionSerializableJit):
             logging.error("Failure to import Metal, please install pyobjc-framework-Metal")
             raise
         import objc
+        dtype_out = vaex.dtype(self.return_dtype).numpy
+        if dtype_out.name == "float64":
+            dtype_out = np.dtype("float32")
+            warnings.warn("Casting output from float64 to float32 since Metal does not support float64")
         ast_node = expresso.parse_expression(self.expression)
         cppcode = node_to_cpp(ast_node)
-        dtype_out = np.dtype(self.dtype_out)
         typemap = {'float32': 'float',
                    'float64': 'float'}  # we downcast!
+        for name in vaex.array_types._type_names_int:
+            typemap[name] = f'{name}_t'
         typenames = [typemap[dtype.name] for dtype in self.argument_dtypes]
         metal_args = [f'const device {typename} *{name}_array [[buffer({i})]]' for i, (typename, name) in
                       enumerate(zip(typenames, self.arguments))]
@@ -49,13 +55,18 @@ using namespace metal;
 float arctan2(float y, float x) {
     return atan2(y, x);
 }
+
+template<typename T>
+T where(bool condition, T y, T x) {
+    return condition ? x : y;
+}
 kernel void vaex_kernel(%s,
-                        device float *vaex_output [[buffer(%i)]],
+                        device %s *vaex_output [[buffer(%i)]],
                         uint id [[thread_position_in_grid]]) {
 %s
     vaex_output[id] = %s;
 }
-''' % (', '.join(metal_args), len(metal_args), ''.join(code_get_scalar), cppcode) # typemap[self.dtype_out],
+''' % (', '.join(metal_args), typemap[dtype_out.name], len(metal_args), ''.join(code_get_scalar), cppcode) # typemap[self.dtype_out],
         if self.verbose:
             print('Generated code:\n' + sourcecode)
         with open('test.metal', 'w') as f:
@@ -70,8 +81,10 @@ kernel void vaex_kernel(%s,
         self.device = Metal.MTLCreateSystemDefaultDevice()
         opts = Metal.MTLCompileOptions.new()
         self.library = self.device.newLibraryWithSource_options_error_(sourcecode, opts, objc.NULL)
-        if self.library is None:
-            logger.error("Error compiling: %s", sourcecode)
+        if self.library[0] is None:
+            msg = f"Error compiling: {sourcecode}, sourcecode"
+            logger.error(msg)
+            raise RuntimeError(msg)
         kernel_name = "vaex_kernel"
         self.vaex_kernel = self.library[0].newFunctionWithName_(kernel_name)
         desc = Metal.MTLComputePipelineDescriptor.new()
@@ -80,13 +93,14 @@ kernel void vaex_kernel(%s,
         command_queue = self.device.newCommandQueue()
 
         def wrapper(*args):
-            args = [ vaex.array_types.to_numpy(ar) for ar in args]
+            args = [vaex.array_types.to_numpy(ar) for ar in args]
             def getbuf(name, value=None, dtype=np.dtype("float32"), N=None):
                 buf = getattr(storage, name, None)
                 if value is not None:
                     N = len(value)
                     dtype = value.dtype
                 if dtype.name == "float64":
+                    warnings.warn("Casting input argument from float64 to float32 since Metal does not support float64")
                     dtype = np.dtype("float32")
                 nbytes = N * dtype.itemsize
                 if buf is not None and buf.length() != nbytes:
@@ -99,11 +113,11 @@ kernel void vaex_kernel(%s,
                 # copy data to buffer
                 if value is not None:
                     mv = buf.contents().as_buffer(buf.length())
-                    buf_as_numpy = np.frombuffer(mv, dtype=np.float32)
-                    buf_as_numpy[:] = value.astype(np.float32, copy=False)
+                    buf_as_numpy = np.frombuffer(mv, dtype=dtype)
+                    buf_as_numpy[:] = value.astype(dtype, copy=False)
                 return buf
             input_buffers = [getbuf(name, chunk) for name, chunk in zip(self.arguments, args)]
-            output_buffer = getbuf('vaex_output', N=len(args[0]))
+            output_buffer = getbuf('vaex_output', N=len(args[0]), dtype=dtype_out)
             buffers = input_buffers + [output_buffer]
             command_buffer = command_queue.commandBuffer()
             encoder = command_buffer.computeCommandEncoder()
