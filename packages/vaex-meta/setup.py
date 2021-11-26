6d00371e81bda5831e66edc6c067ba139e9bedeb
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-11-26 14:37:07 +0100

    🔖 vaex-ml 0.15.0 released

--- packages/vaex-meta/setup.py
+++ packages/vaex-meta/setup.py
@@ -18,11 +18,11 @@ url = 'https://www.github.com/vaexio/vaex'
 install_requires = [
       'vaex-core>=4.6.0,<5',
       'vaex-viz>=0.5.0,<0.6',
-      'vaex-server>=0.7.0,<0.7',
+      'vaex-server>=0.7.0,<0.8',
       'vaex-hdf5>=0.11.0,<0.12',
       'vaex-astro>=0.9.0,<0.10',
       'vaex-jupyter>=0.6.0,<0.7',
-      'vaex-ml>=0.14.0,<0.15',
+      'vaex-ml>=0.15.0,<0.16',
       # vaex-graphql is not on conda-forge yet
 ]
 
