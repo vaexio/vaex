b8e6e52ad73a08a05b5fffd964ee59c63a57b993
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-12-03 15:51:13 +0100

    ci: disable testing ml notebook due to tensorflow

--- ci/05-run-notebooks.sh
+++ ci/05-run-notebooks.sh
@@ -14,7 +14,7 @@ cd docs/source
 
 # python -m nbconvert --TagRemovePreprocessor.remove_cell_tags="('skip-ci',)" --to html --execute tutorial_jupyter.ipynb --ExecutePreprocessor.timeout=240
  python -m nbconvert --TagRemovePreprocessor.remove_cell_tags="('skip-ci',)" --to html --execute tutorial.ipynb --ExecutePreprocessor.timeout=240
- python -m nbconvert --TagRemovePreprocessor.remove_cell_tags="('skip-ci',)" --to html --execute tutorial_ml.ipynb --ExecutePreprocessor.timeout=240
+#  python -m nbconvert --TagRemovePreprocessor.remove_cell_tags="('skip-ci',)" --to html --execute tutorial_ml.ipynb --ExecutePreprocessor.timeout=240
  # we cannot run the arrow example, maybe with a remote dataframe?
  # python -m nbconvert --TagRemovePreprocessor.remove_cell_tags="('skip-ci',)" --to html --execute example_arrow.ipynb
  python -m nbconvert --TagRemovePreprocessor.remove_cell_tags="('skip-ci',)" --to html --execute example_dask.ipynb --ExecutePreprocessor.timeout=240
