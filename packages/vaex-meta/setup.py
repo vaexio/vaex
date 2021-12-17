dd62220c0a757a6d2b5c2b3c790accc9495e9f93
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-12-17 20:46:21 +0100

    🔖 vaex-jupyter 0.7.0 released

--- packages/vaex-meta/setup.py
+++ packages/vaex-meta/setup.py
@@ -21,7 +21,7 @@ install_requires = [
       'vaex-hdf5>=0.11.1,<0.12',
       'vaex-viz>=0.5.0,<0.6',
       'vaex-server>=0.8.0,<0.8',
-      'vaex-jupyter>=0.6.0,<0.7',
+      'vaex-jupyter>=0.7.0,<0.7',
       'vaex-ml>=0.15.0,<0.16',
       # vaex-graphql is not on conda-forge yet
 ]
