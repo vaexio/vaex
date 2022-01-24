6b542c8b5eaa54e60b47fc0a050739b8a2be1f9c
Maarten Breddels <maartenbreddels@gmail.com>
2022-01-24 20:09:01 +0100

    🐛 frozendict 2.2 on Python 3.6 segfaults (#1856)
    See https://github.com/Marco-Sulla/python-frozendict/issues/48

--- packages/vaex-core/setup.py
+++ packages/vaex-core/setup.py
@@ -29,7 +29,7 @@ url = 'https://www.github.com/maartenbreddels/vaex'
 install_requires_core = ["numpy>=1.16", "aplus", "tabulate>=0.8.3",
                          "future>=0.15.2", "pyyaml", "progressbar2",
                          "requests", "six", "cloudpickle", "pandas", "dask",
-                         "nest-asyncio>=1.3.3", "pyarrow>=3.0", "frozendict",
+                         "nest-asyncio>=1.3.3", "pyarrow>=3.0", "frozendict!=2.2.0",
                          "blake3", "filelock", "pydantic>=1.8.0", "rich",
                         ]
 if sys.version_info[0] == 2:
