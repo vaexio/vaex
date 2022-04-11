vaexio/vaex

commit 6e4f0d92a4da6ddb5d41d3748f2003785e787f8a

fix(core): Do not duplicate columns when joining DataFrames on columns with the identical name.

tests/join_test.py
@@ -202,3 +202,15 @@ def test_join_filtered_inner():
     df = df[df.x > 5]
     dfj = df.join(df, on='x', rsuffix='right_', how='inner')
     repr(dfj)  # trigger issue with selection cache
+
+
+def test_join_duplicate_column():
+    df_left = vaex.from_arrays(index=[1, 2, 3], x=[10, 20, 30])
+    df_right = vaex.from_arrays(index=[1, 2, 3], y=[0.1, 0.2, 0.3])
+
+    df = df_left.join(df_right, on='index')
+    assert df.column_count() == 3
+    assert set(df.column_names) == {'index', 'x', 'y'}
+    assert df['index'] == [1, 2, 3]
+    assert df.x.tolist() == [10, 20, 30]
+    assert df.y.tolist() == [0.1, 0.2, 0.3]

packages/vaex-core/vaex/core/_version.py
@@ -1,2 +1,2 @@
-__version_tuple__ = (1, 3, 0)
-__version__ = '1.3.0'
+__version_tuple__ = (1, 3, 1, 'dev')
+__version__ = '1.3.1-dev'

packages/vaex-core/vaex/dataframe.py
@@ -5365,7 +5365,11 @@ class DataFrameLocal(DataFrame):
             raise ValueError('join type not supported: {}, only left and right'.format(how))
         left = left if inplace else left.copy()
 
+        left_on = left_on or on
+        right_on = right_on or on
         for name in right:
+            if left_on and (rprefix + name + rsuffix == lprefix + left_on + lsuffix):
+                continue  # it's ok when we join on the same column name
             if name in left and rprefix + name + rsuffix == lprefix + name + lsuffix:
                 raise ValueError('column name collision: {} exists in both column, and no proper suffix given'
                                  .format(name))
@@ -5374,8 +5378,6 @@ class DataFrameLocal(DataFrame):
         assert left.length_unfiltered() == left.length_original()
         N = left.length_unfiltered()
         N_other = len(right)
-        left_on = left_on or on
-        right_on = right_on or on
         if left_on is None and right_on is None:
             for name in right:
                 right_name = name
@@ -5434,6 +5436,8 @@ class DataFrameLocal(DataFrame):
                 lookup = np.ma.array(lookup, mask=lookup==-1)
             direct_indices_map = {}  # for performance, keeps a cache of two levels of indirection of indices
             for name in right:
+                if rprefix + name + rsuffix == lprefix + left_on + lsuffix:
+                    continue  # skip when it's the join column
                 right_name = name
                 if name in left:
                     left.rename_column(name, lprefix + name + lsuffix)

packages/vaex-meta/setup.py
@@ -16,7 +16,7 @@ version = version.__version__
 url = 'https://www.github.com/maartenbreddels/vaex'
 
 install_requires = [
-      'vaex-core>=1.3.0,<2',
+      'vaex-core>=1.3.1-dev,<2',
       'vaex-viz>=0.3.8,<0.4',
       'vaex-server>=0.2.1,<0.3',
       'vaex-hdf5>=0.5.6,<0.6',

packages/vaex-meta/vaex/meta/_version.py
@@ -1,2 +1,2 @@
-__version_tuple__ = (2, 4, 0)
-__version__ = '2.4.0'
+__version_tuple__ = (2, 4, 1, 'dev')
+__version__ = '2.4.1-dev'

commit 5fe03b1a7d8b01d54303fe608b1cf0476bf25f3f

fix: join would fail if the rhs had no columns besides the join one
Probably introduced in #480

packages/vaex-core/vaex/join.py
@@ -1,4 +1,5 @@
 import numpy as np
+from vaex.dataframe import DataFrame
 
 import vaex.dataset
 from vaex.utils import _ensure_string_from_expression
@@ -124,8 +125,8 @@ class DatasetJoin(vaex.dataset.DatasetDecorator):
 def join(df, other, on=None, left_on=None, right_on=None, lprefix='', rprefix='', lsuffix='', rsuffix='', how='left', allow_duplication=False, prime_growth=False, cardinality_other=None, inplace=False):
     # implementation of DataFrameLocal.join
     inner = False
-    left = df
-    right = other
+    left: DataFrame = df
+    right: DataFrame = other
     left_original = left.copy()
     right_original = right.copy()
     rprefix_original, lprefix_original = rprefix, lprefix
@@ -276,13 +277,16 @@ def join(df, other, on=None, left_on=None, right_on=None, lprefix='', rprefix=''
             left.column_names.append(name)
             left._initialize_column(name)
     # merge the two datasets
-    right_dataset = right.dataset.project(*right_columns)
-    if lookup is not None:
-        # if lookup is None, we do a row based join
-        # and we only need to merge.
-        # if we have an array of lookup indices, we 'take' those
-        right_dataset = right_dataset.take(lookup, masked=any(lookup_masked))
-    dataset = left.dataset.merged(right_dataset)
+    if right_columns:
+        right_dataset = right.dataset.project(*right_columns)
+        if lookup is not None:
+            # if lookup is None, we do a row based join
+            # and we only need to merge.
+            # if we have an array of lookup indices, we 'take' those
+            right_dataset = right_dataset.take(lookup, masked=any(lookup_masked))
+        dataset = left.dataset.merged(right_dataset)
+    else:
+        dataset = left.dataset
     # row number etc should not have changed, we only append new columns
     # so no need to reset caches
     left._dataset = DatasetJoin(dataset, left_original, right_original,

tests/join_test.py
@@ -384,3 +384,11 @@ def test_apply_function_name_collision():
     df2["y2"] = df2["y"].apply(transform, multiprocessing=False)
     joined = df1.join(df2, left_on="x2", right_on="y2", allow_duplication=True)
     assert joined.y2.tolist() == [2, 99]
+
+
+def test_join_no_right_columns_left():
+    df1 = vaex.from_arrays(a=[1, 2, 3])
+    # df2 only contains 'a', so we don't do the join for real
+    df2 = vaex.from_arrays(a=[1, 10])
+    df = df1.join(df2, on="a", how="inner")
+    assert df["a"].tolist() == [1]