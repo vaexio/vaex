baa8ba0b0ef9e97b4867345cbfe51ed7e7a88c53
franz.media <fkrekeler@gmail.com>
2022-10-10 17:56:04 -0500

    moving to importlib

--- packages/vaex-core/vaex/version.py
+++ packages/vaex-core/vaex/version.py
@@ -1,6 +1,3 @@
-import pkg_resources
-
-
 packages = ['vaex', 'vaex-core', 'vaex-viz', 'vaex-hdf5', 'vaex-server', 'vaex-astro',
             'vaex-ui', 'vaex-jupyter', 'vaex-ml', 'vaex-arrow', 'vaex-graphql', 'vaex-enterprise']
 
@@ -8,8 +5,23 @@ packages = ['vaex', 'vaex-core', 'vaex-viz', 'vaex-hdf5', 'vaex-server', 'vaex-a
 def get_versions():
     def is_installed(p):
         try:
-            pkg_resources.get_distribution(p)
-            return True
-        except pkg_resources.DistributionNotFound:
-            return False
+            import importlib.metadata
+        except ImportError:
+            try:
+                from pkg_resources import DistributionNotFound
+                from pkg_resources import get_distribution
+            except ImportError:  # pragma: no cover
+                pass
+            else:
+                try:
+                    get_distribution(p).version
+                    return True
+                except DistributionNotFound:  # pragma: no cover
+                    return False
+        else:
+            try:
+                importlib.metadata.version(p)
+                return True
+            except importlib.metadata.PackageNotFoundError:  # pragma: no cover
+                return False
     return {p: pkg_resources.get_distribution(p).version for p in packages if is_installed(p)}
