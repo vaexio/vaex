3f087f1df3094dcb5ed39c11f4db0cdb1ce20b15
franz.media <fkrekeler@gmail.com>
2022-10-12 14:32:53 -0500

    Update version.py

--- packages/vaex-core/vaex/version.py
+++ packages/vaex-core/vaex/version.py
@@ -1,7 +1,6 @@
 packages = ['vaex', 'vaex-core', 'vaex-viz', 'vaex-hdf5', 'vaex-server', 'vaex-astro',
             'vaex-ui', 'vaex-jupyter', 'vaex-ml', 'vaex-arrow', 'vaex-graphql', 'vaex-enterprise']
 
-
 def get_versions():
     def is_installed(p):
         #  Concept from https://github.com/geoalchemy/geoalchemy2/pull/392/files#diff-208c493ee84f787a0a966b235a31f86a5d382ff76e1e5533b5b1da712996b7e2
@@ -15,14 +14,25 @@ def get_versions():
                 pass
             else:
                 try:
-                    get_distribution(p).version
-                    return True
+                    return get_distribution(p).version
                 except DistributionNotFound:  # pragma: no cover
-                    return False
+                    raise Exception(f"Package not found: {p}")
         else:
             try:
-                importlib.metadata.version(p)
-                return True
+                return importlib.metadata.version(p)
+
             except importlib.metadata.PackageNotFoundError:  # pragma: no cover
-                return False
-    return {p: pkg_resources.get_distribution(p).version for p in packages if is_installed(p)}
+                raise Exception(f"Package not found: {p}")
+
+    package_versions = {}
+
+    for p in packages:
+        try:
+            version = is_installed(p)
+            package_versions[p] = version
+        except Exception as e:
+            # check exception text contains 'Package not found: '
+            if 'Package not found: ' not in str(e):
+                raise e
+
+    return package_versions
