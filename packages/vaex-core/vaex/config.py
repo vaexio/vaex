e43ac6ec1b6a7409684dc5ebc3dd1ccfe958b593
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-07-07 14:24:05 +0200

    fix: make compatible with pydantic 2

--- packages/vaex-core/vaex/config.py
+++ packages/vaex-core/vaex/config.py
@@ -1,9 +1,22 @@
 import os
-from pydantic import BaseSettings
 import vaex.utils
-
+import pydantic
 
 # in seperate module to avoid circular imports
+# with pydantic 2.0, we require pydantic_settings
+try:
+    import pydantic_settings
+except ModuleNotFoundError:
+    # we should be on pydantic 1.x
+    BaseSettings = pydantic.BaseSettings
+else:
+    major = pydantic_settings.__version__.split(".")[0]
+    if major != "0":
+        # but the old pydantic_settings is unrelated
+        BaseSettings = pydantic_settings.BaseSettings
+    else:
+        # we should be on pydantic 2.x
+        BaseSettings = pydantic.BaseSettings
 
 class ConfigDefault:
     env_file = ".env"
--- packages/vaex-server/vaex/server/settings.py
+++ packages/vaex-server/vaex/server/settings.py
@@ -1,7 +1,24 @@
 
 from typing import Dict
-from pydantic import BaseSettings, Field
+from pydantic import Field
+import pydantic
 import vaex.config
+
+# with pydantic 2.0, we require pydantic_settings
+try:
+    import pydantic_settings
+except ModuleNotFoundError:
+    # we should be on pydantic 1.x
+    BaseSettings = pydantic.BaseSettings
+else:
+    major = pydantic_settings.__version__.split(".")[0]
+    if major != "0":
+        # but the old pydantic_settings is unrelated
+        BaseSettings = pydantic_settings.BaseSettings
+    else:
+        # we should be on pydantic 2.x
+        BaseSettings = pydantic.BaseSettings
+
 
 class Settings(BaseSettings):
     '''Configuration options for the FastAPI server'''
@@ -9,7 +26,7 @@ class Settings(BaseSettings):
     add_example: bool = Field(True, title="Add example dataset")
     # vaex_config: dict = None
     graphql: bool = Field(False, title="Add graphql endpoint")
-    files: Dict[str, str] = Field(default_factory=list, title="Mapping of name to path")
+    files: Dict[str, str] = Field(default_factory=dict, title="Mapping of name to path")
     class Config(vaex.config.ConfigDefault):
         env_file = '.env'
         env_file_encoding = 'utf-8'
