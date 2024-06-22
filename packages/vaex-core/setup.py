12e247872d7780a05debb753f584e77d5b3e22f7
Maarten Breddels <maartenbreddels@gmail.com>
2024-06-22 16:00:42 +0200

    Update packages/vaex-core/setup.py
    Co-authored-by: ddelange <14880945+ddelange@users.noreply.github.com>

--- packages/vaex-core/setup.py
+++ packages/vaex-core/setup.py
@@ -183,7 +183,7 @@ setup(name=name + '-core',
       include_package_data=True,
       ext_modules=([extension_vaexfast] if on_rtd else [extension_vaexfast, extension_strings, extension_superutils, extension_superagg]) if not use_skbuild else [],
       zip_safe=False,
-      python_requires=">=3.6",
+      python_requires=">=3.8",
       classifiers=[
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
